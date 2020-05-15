5f28574d30a4f7e1bf7aa204fb8d5f3a9491cbe1
Hernand Azevedo <hernand.azevedo@zup.com.br>
2020-05-15 10:45:03 -0300

    :lipstick: Improves code

---
+++ android/android-processor/src/main/java/br/com/zup/beagle/compiler/BeagleWidgetBindingAdapterGenerator.kt
@@ -0,0 +1,150 @@
+/*
+ * Copyright 2020 ZUP IT SERVICOS EM TECNOLOGIA E INOVACAO SA
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package br.com.zup.beagle.compiler
+
+import br.com.zup.beagle.compiler.util.BINDING
+import br.com.zup.beagle.compiler.util.isMarkedNullable
+import com.squareup.kotlinpoet.ClassName
+import com.squareup.kotlinpoet.FunSpec
+import com.squareup.kotlinpoet.KModifier
+import com.squareup.kotlinpoet.ParameterizedTypeName.Companion.parameterizedBy
+import com.squareup.kotlinpoet.PropertySpec
+import com.squareup.kotlinpoet.STAR
+import com.squareup.kotlinpoet.asClassName
+import javax.annotation.processing.ProcessingEnvironment
+import javax.lang.model.element.Element
+import javax.lang.model.element.ElementKind
+import javax.lang.model.element.ExecutableElement
+
+private const val WIDGET_PROPERTY = "widget"
+private const val BINDING_PROPERTY = "binding"
+private const val BIND_MODEL_METHOD = "bindModel"
+private const val ADAPTER_SUFFIX = "BindingAdapter"
+private const val GET_VALUE_NULL_METHOD = "getValueNull"
+private const val GET_VALUE_NOT_NULL_METHOD = "getValueNotNull"
+private const val GET_BIND_ATTRIBUTES_METHOD = "getBindAttributes"
+
+class BeagleWidgetBindingAdapterGenerator(private val processingEnv: ProcessingEnvironment) {
+    fun getPropertyBinding(element: Element) = addConstructorParameter(element)
+
+    fun getPropertyWidget(element: Element): PropertySpec {
+        val packageElement = getPackageOf(element)
+        val bindingClassName = "${element.simpleName}"
+        return PropertySpec.builder(WIDGET_PROPERTY, ClassName(
+            packageElement,
+            bindingClassName
+        )).initializer(WIDGET_PROPERTY).build()
+    }
+
+    fun getFunctionBindModel(element: Element): FunSpec {
+        val attributeValues = StringBuilder()
+        val notifyValues = StringBuilder()
+
+        val constructorParameters = listConstructorParameters(element)
+        constructorParameters.forEachIndexed { index, e ->
+            val isNullable = isMarkedNullable(e)
+            val getValueMethodName = if (isNullable) GET_VALUE_NULL_METHOD else GET_VALUE_NOT_NULL_METHOD
+
+            val attr = e.simpleName
+            attributeValues.append("\t$attr = ${getValueMethodName}($BINDING_PROPERTY.$attr, $WIDGET_PROPERTY.$attr)")
+
+            if (index < constructorParameters.size - 1) {
+                attributeValues.append(",\n")
+            }
+        }
+
+        constructorParameters.forEachIndexed { index, e ->
+            val attr = e.simpleName
+            notifyValues.append("""
+                |$BINDING_PROPERTY.$attr.observes {
+                |   myWidget = myWidget.copy($attr = it)
+                |   $WIDGET_PROPERTY.onBind(myWidget)
+                |}
+                |
+            """.trimMargin())
+        }
+        return FunSpec.builder(BIND_MODEL_METHOD)
+            .addModifiers(KModifier.OVERRIDE)
+            .addCode("""
+                |var myWidget = ${element.simpleName}(
+                |$attributeValues
+                |)
+                |$WIDGET_PROPERTY.onBind(myWidget)
+                |   $notifyValues
+                |""".trimMargin())
+            .build()
+    }
+
+    fun getBindingAdapterClassNameForWidget(element: Element) =
+        "${element.simpleName}$ADAPTER_SUFFIX"
+
+    fun getPackageOf(element: Element) = processingEnv.elementUtils.getPackageOf(element).toString()
+
+    private fun addConstructorParameter(element: Element): PropertySpec {
+        val packageElement = getPackageOf(element)
+        val bindingClassName = "${element.simpleName}Binding"
+        return PropertySpec.builder(BINDING_PROPERTY, ClassName(
+            packageElement,
+            bindingClassName
+        )).initializer(BINDING_PROPERTY).build()
+    }
+
+    fun getFunctionGetBindAttributes(element: Element): FunSpec {
+
+        val returnType = List::class.asClassName().parameterizedBy(
+            ClassName(BINDING.packageName, BINDING.className)
+                .parameterizedBy(STAR)
+        )
+        val attributeValues = StringBuilder()
+
+        val constructorParameters = listConstructorParameters(element)
+        constructorParameters.forEachIndexed { index, e ->
+            attributeValues.append("\t$BINDING_PROPERTY.${e.simpleName}")
+            if (index < constructorParameters.size - 1) {
+                attributeValues.append(",\n")
+            }
+        }
+
+        return FunSpec.builder(GET_BIND_ATTRIBUTES_METHOD)
+            .addModifiers(KModifier.OVERRIDE)
+            .addCode("""
+                        |val bindAttributes = listOf(
+                        |   $attributeValues
+                        |)
+                    |""".trimMargin())
+            .addStatement("return bindAttributes")
+            .returns(returnType)
+            .build()
+    }
+
+    fun getConstructorParametersSpec(element: Element): FunSpec.Builder {
+        val packageElement = getPackageOf(element)
+        val bindingClassName = "${element.simpleName}Binding"
+        return FunSpec.constructorBuilder()
+            .addParameter(WIDGET_PROPERTY, ClassName(
+                packageElement,
+                element.simpleName.toString()
+            ))
+            .addParameter(BINDING_PROPERTY, ClassName(
+                packageElement,
+                bindingClassName
+            ))
+    }
+
+    private fun listConstructorParameters(element: Element) =
+        (element.enclosedElements.first { it.kind == ElementKind.CONSTRUCTOR } as ExecutableElement).parameters
+}
--- android/android-processor/src/main/java/br/com/zup/beagle/compiler/BeagleWidgetBindingAdapterProcessor.kt
+++ android/android-processor/src/main/java/br/com/zup/beagle/compiler/BeagleWidgetBindingAdapterProcessor.kt
@@ -17,62 +17,57 @@
 package br.com.zup.beagle.compiler
 
 import br.com.zup.beagle.annotation.RegisterWidget
-import br.com.zup.beagle.compiler.util.BINDING
 import br.com.zup.beagle.compiler.util.BINDING_ADAPTER
 import br.com.zup.beagle.compiler.util.GET_VALUE_NOT_NULL
 import br.com.zup.beagle.compiler.util.GET_VALUE_NULL
 import br.com.zup.beagle.compiler.util.error
-import br.com.zup.beagle.compiler.util.isMarkedNullable
 import com.squareup.kotlinpoet.ClassName
 import com.squareup.kotlinpoet.FileSpec
-import com.squareup.kotlinpoet.FunSpec
 import com.squareup.kotlinpoet.KModifier
-import com.squareup.kotlinpoet.ParameterizedTypeName.Companion.parameterizedBy
-import com.squareup.kotlinpoet.PropertySpec
-import com.squareup.kotlinpoet.STAR
 import com.squareup.kotlinpoet.TypeSpec
-import com.squareup.kotlinpoet.asClassName
 import java.io.IOException
 import javax.annotation.processing.ProcessingEnvironment
 import javax.annotation.processing.RoundEnvironment
-import javax.lang.model.element.Element
-import javax.lang.model.element.ElementKind
-import javax.lang.model.element.ExecutableElement
-import javax.lang.model.element.PackageElement
 
 class BeagleWidgetBindingAdapterProcessor(
-    private val processingEnv: ProcessingEnvironment
+    private val processingEnv: ProcessingEnvironment,
+    private val beagleWidgetBindingAdapterGenerator: BeagleWidgetBindingAdapterGenerator =
+        BeagleWidgetBindingAdapterGenerator(
+            processingEnv
+        )
 ) {
 
     fun process(
         roundEnvironment: RoundEnvironment
     ) {
-        val registerWidgetAnnotatedClasses = roundEnvironment.getElementsAnnotatedWith(RegisterWidget::class.java)
+        val registerWidgetAnnotatedClasses = roundEnvironment
+            .getElementsAnnotatedWith(RegisterWidget::class.java)
         registerWidgetAnnotatedClasses.forEachIndexed { _, element ->
 
-            val bindingAdapterClassName = "${element.simpleName}BindingAdapter"
+            val bindingAdapterClassName = beagleWidgetBindingAdapterGenerator
+                .getBindingAdapterClassNameForWidget(element)
 
             val typeSpec = TypeSpec.classBuilder(bindingAdapterClassName)
                 .addModifiers(KModifier.PUBLIC, KModifier.FINAL)
-                .primaryConstructor(constructorParameters(element)
+                .primaryConstructor(beagleWidgetBindingAdapterGenerator.getConstructorParametersSpec(element)
                     .build())
-                .addProperty(propertyWidget(element))
-                .addProperty(propertyBinding(element))
+                .addProperty(beagleWidgetBindingAdapterGenerator.getPropertyWidget(element))
+                .addProperty(beagleWidgetBindingAdapterGenerator.getPropertyBinding(element))
                 .addSuperinterface(ClassName(
                     BINDING_ADAPTER.packageName,
                     BINDING_ADAPTER.className
                 ))
                 .addFunction(
-                    functionBindModel(element)
+                    beagleWidgetBindingAdapterGenerator.getFunctionBindModel(element)
                 )
                 .addFunction(
-                    functionGetBindAttributes(element)
+                    beagleWidgetBindingAdapterGenerator.getFunctionGetBindAttributes(element)
                 )
                 .build()
-            val packageElement: PackageElement = processingEnv.elementUtils.getPackageOf(element)
+            val packageElement = beagleWidgetBindingAdapterGenerator.getPackageOf(element)
 
             val beagleSetupFile = FileSpec.builder(
-                packageElement.toString(),
+                packageElement,
                 bindingAdapterClassName
             ).addImport(GET_VALUE_NULL.packageName, GET_VALUE_NULL.className)
                 .addImport(GET_VALUE_NOT_NULL.packageName, GET_VALUE_NOT_NULL.className)
@@ -87,114 +82,5 @@ class BeagleWidgetBindingAdapterProcessor(
             }
         }
 
-
-    }
-
-    private fun propertyBinding(element: Element) = addConstructorParameter(element)
-
-    private fun propertyWidget(element: Element): PropertySpec {
-        val packageElement: PackageElement = processingEnv.elementUtils.getPackageOf(element)
-        val bindingClassName = "${element.simpleName}"
-        return PropertySpec.builder("widget", ClassName(
-            packageElement.toString(),
-            bindingClassName
-        )).initializer("widget").build()
-    }
-
-    private fun functionBindModel(element: Element): FunSpec {
-        val attributeValues = StringBuilder()
-        val notifyValues = StringBuilder()
-        val packageElement: PackageElement = processingEnv.elementUtils.getPackageOf(element)
-        val widgetClassName = "${element.simpleName}"
-
-        val widgetClass = "$packageElement.${element.simpleName}"
-
-        val constructorParameters = getConstructorParameters(element)
-        constructorParameters.forEachIndexed { index, e ->
-            val isNullable = isMarkedNullable(e)
-            val getValueMethodName = if (isNullable) "getValueNull" else "getValueNotNull"
-
-            val attr = e.simpleName
-            attributeValues.append("\t$attr = ${getValueMethodName}(binding.$attr, widget.$attr)")
-
-            if (index < constructorParameters.size - 1) {
-                attributeValues.append(",\n")
-            }
-        }
-
-        constructorParameters.forEachIndexed { index, e ->
-            val attr = e.simpleName
-            notifyValues.append("""
-                |binding.$attr.observes {
-                |   myWidget = myWidget.copy($attr = it)
-                |   widget.onBind(myWidget)
-                |}
-                |
-            """.trimMargin())
-        }
-        return FunSpec.builder("bindModel")
-            .addModifiers(KModifier.OVERRIDE)
-            .addCode("""
-                |var myWidget = ${element.simpleName}(
-                |$attributeValues
-                |)
-                |widget.onBind(myWidget)
-                |   $notifyValues
-                |""".trimMargin())
-            .build()
-    }
-
-    private fun addConstructorParameter(element: Element): PropertySpec {
-        val packageElement: PackageElement = processingEnv.elementUtils.getPackageOf(element)
-        val bindingClassName = "${element.simpleName}Binding"
-        return PropertySpec.builder("binding", ClassName(
-            packageElement.toString(),
-            bindingClassName
-        )).initializer("binding").build()
-    }
-
-    private fun functionGetBindAttributes(element: Element): FunSpec {
-
-        val returnType = List::class.asClassName().parameterizedBy(
-            ClassName(BINDING.packageName, BINDING.className)
-                .parameterizedBy(STAR)
-        )
-        val attributeValues = StringBuilder()
-
-        val constructorParameters = getConstructorParameters(element)
-        constructorParameters.forEachIndexed { index, e ->
-            attributeValues.append("\tbinding.${e.simpleName}")
-            if (index < constructorParameters.size - 1) {
-                attributeValues.append(",\n")
-            }
-        }
-
-        return FunSpec.builder("getBindAttributes")
-            .addModifiers(KModifier.OVERRIDE)
-            .addCode("""
-                        |val bindAttributes = listOf(
-                        |   $attributeValues
-                        |)
-                    |""".trimMargin())
-            .addStatement("return bindAttributes")
-            .returns(returnType)
-            .build()
-    }
-
-    private fun getConstructorParameters(element: Element) =
-        (element.enclosedElements.first { it.kind == ElementKind.CONSTRUCTOR } as ExecutableElement).parameters
-
-    private fun constructorParameters(element: Element): FunSpec.Builder {
-        val packageElement: PackageElement = processingEnv.elementUtils.getPackageOf(element)
-        val bindingClassName = "${element.simpleName}Binding"
-        return FunSpec.constructorBuilder()
-            .addParameter("widget", ClassName(
-                packageElement.toString(),
-                element.simpleName.toString()
-            ))
-            .addParameter("binding", ClassName(
-                packageElement.toString(),
-                bindingClassName
-            ))
     }
 }
