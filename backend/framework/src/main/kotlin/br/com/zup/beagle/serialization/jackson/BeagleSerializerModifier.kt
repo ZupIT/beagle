283a811d46ec538b7a1ef2f3ce458ee4daa1b478
Luiz Gustavo Gomes Borges <luiz.g.borges95@gmail.com>
2020-05-19 16:47:05 -0300

    feat: serializer for binding that treats it as a union of String and T (#111)
    * Defined serializer for binding type that treats it as a union type of String and T.
    
    * Simplified binding and builder serialization with mixins.
    
    * Added unit testing for mixins.
    
    * Removed unused method.

--- backend/framework/src/main/kotlin/br/com/zup/beagle/serialization/jackson/BeagleSerializerModifier.kt
+++ backend/framework/src/main/kotlin/br/com/zup/beagle/serialization/jackson/BeagleSerializerModifier.kt
@@ -18,9 +18,7 @@ package br.com.zup.beagle.serialization.jackson
 
 import br.com.zup.beagle.action.Action
 import br.com.zup.beagle.core.ServerDrivenComponent
-import br.com.zup.beagle.widget.core.ComposeComponent
 import br.com.zup.beagle.widget.layout.Screen
-import br.com.zup.beagle.widget.layout.ScreenBuilder
 import com.fasterxml.jackson.databind.BeanDescription
 import com.fasterxml.jackson.databind.JsonSerializer
 import com.fasterxml.jackson.databind.SerializationConfig
@@ -28,27 +26,16 @@ import com.fasterxml.jackson.databind.ser.BeanSerializerModifier
 import com.fasterxml.jackson.databind.ser.std.BeanSerializerBase
 
 internal object BeagleSerializerModifier : BeanSerializerModifier() {
-    private val beagleBaseClasses = listOf(Action::class.java, Screen::class.java, ServerDrivenComponent::class.java)
-
-    private val beagleBuilders = listOf(ComposeComponent::class.java, ScreenBuilder::class.java)
+    private val beagleBaseClasses = arrayOf(Action::class, Screen::class, ServerDrivenComponent::class)
 
     override fun modifySerializer(
         config: SerializationConfig,
         description: BeanDescription,
         serializer: JsonSerializer<*>
     ) =
-        if (serializer is BeanSerializerBase && isBeagleBuilder(description)) {
-            BeagleBuilderSerializer(serializer)
-        } else if (serializer is BeanSerializerBase && isBeagleBase(description)) {
-            BeagleTypeSerializer(serializer)
-        } else {
-            serializer
-        }
+        if (serializer is BeanSerializerBase && this.isBeagleBase(description)) BeagleTypeSerializer(serializer)
+        else serializer
 
-    private fun isBeagleBase(description: BeanDescription) = beagleBaseClasses.findAssignableFrom(description)
-
-    private fun isBeagleBuilder(description: BeanDescription) = beagleBuilders.findAssignableFrom(description)
-
-    private fun List<Class<*>>.findAssignableFrom(description: BeanDescription) =
-        this.find { it.isAssignableFrom(description.beanClass) } != null
+    private fun isBeagleBase(description: BeanDescription) =
+        beagleBaseClasses.any { it.java.isAssignableFrom(description.beanClass) }
 }
