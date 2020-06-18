0219992ed2e23726c6c524b4c719f07b31c7a188
Túlio Magalhães <tulio.pereira@zup.com.br>
2020-06-18 10:11:19 -0300

    Create multiple expression logic

--- android/beagle/src/main/java/br/com/zup/beagle/android/context/ContextDataManager.kt
+++ android/beagle/src/main/java/br/com/zup/beagle/android/context/ContextDataManager.kt
@@ -21,10 +21,12 @@ import br.com.zup.beagle.android.data.serializer.BeagleMoshi
 import br.com.zup.beagle.android.jsonpath.JsonPathFinder
 import br.com.zup.beagle.android.jsonpath.JsonPathReplacer
 import br.com.zup.beagle.android.logger.BeagleMessageLogs
+import br.com.zup.beagle.android.utils.getExpressions
 import com.squareup.moshi.Moshi
 import org.json.JSONArray
 import org.json.JSONObject
 import java.lang.IllegalStateException
+import java.lang.reflect.Type
 
 private val EXPRESSION_REGEX = "@\\{([^)]+)\\}".toRegex()
 
@@ -54,8 +56,10 @@ internal class ContextDataManager(
     }
 
     fun addBindingToContext(binding: Bind.Expression<*>) {
-        val contextId = binding.getContextId()
-        contexts[contextId]?.bindings?.add(binding)
+        binding.value.getExpressions().forEach { bindingValue ->
+            val contextId = bindingValue.split(".")[0]
+            contexts[contextId]?.bindings?.add(binding)
+        }
     }
 
     fun updateContext(setContextInternal: SetContextInternal): Boolean {
@@ -79,48 +83,13 @@ internal class ContextDataManager(
         val contextId = bind.getContextId()
 
         return contexts[contextId]?.let {
-            return evaluateBinding(it.context, bind)
-        }
-    }
-
-    private fun evaluateBinding(contextData: ContextData, bind: Bind.Expression<*>): Any? {
-        val value = getValue(contextData, bind.valueInExpression())
-
-        return try {
-            if (value is JSONArray || value is JSONObject) {
-                moshi.adapter<Any>(bind.type).fromJson(value.toString()) ?:
-                throw IllegalStateException("JSON deserialization returned null")
-            } else {
-                value ?: throw IllegalStateException("Expression evaluation returned null")
-            }
-        } catch (ex: Exception) {
-            BeagleMessageLogs.errorWhileTryingToNotifyContextChanges(ex)
-            null
+            return evaluateBindExpression(it.context, bind)
         }
     }
 
     private fun evaluateContext(contextId: String) {
         contexts[contextId]?.let {
             notifyBindingChanges(it)
-        }
-    }
-
-    private fun getValue(contextData: ContextData, path: String): Any? {
-        return if (path != contextData.id) {
-            findAndCacheValue(contextData, path)
-        } else {
-            contextData.value
-        }
-    }
-
-    private fun findAndCacheValue(contextData: ContextData, path: String): Any? {
-        return try {
-            val keys = contextPathResolver.getKeysFromPath(contextData.id, path)
-            val foundValue = jsonPathFinder.find(keys, contextData.value)
-            foundValue
-        } catch (ex: Exception) {
-            BeagleMessageLogs.errorWhileTryingToAccessContext(ex)
-            null
         }
     }
 
@@ -146,10 +115,61 @@ internal class ContextDataManager(
         val bindings = contextBinding.bindings
 
         bindings.forEach { bind ->
-            val value = evaluateBinding(contextData, bind)
+            val value = evaluateBindExpression(contextData, bind)
+
             if (value != null) {
                 bind.notifyChange(value)
             }
+        }
+    }
+
+    private fun evaluateBindExpression(contextData: ContextData, bind: Bind.Expression<*>): Any? {
+        val expressions = bind.value.getExpressions()
+
+        return if (bind.type == String::class.java) {
+            var text = bind.value
+            expressions.forEach {
+                val value = evaluateExpression(contextData, bind.type, it)
+                text = text.replace("@\\{$it\\}".toRegex(), value.toString())
+            }
+            text
+        } else {
+            evaluateExpression(contextData, bind.type, expressions[0])
+        }
+    }
+
+    private fun evaluateExpression(contextData: ContextData, type: Type, expression: String): Any? {
+        val value = getValue(contextData, expression)
+
+        return try {
+            if (value is JSONArray || value is JSONObject) {
+                moshi.adapter<Any>(type).fromJson(value.toString()) ?:
+                throw IllegalStateException("JSON deserialization returned null")
+            } else {
+                value ?: throw IllegalStateException("Expression evaluation returned null")
+            }
+        } catch (ex: Exception) {
+            BeagleMessageLogs.errorWhileTryingToNotifyContextChanges(ex)
+            null
+        }
+    }
+
+    private fun getValue(contextData: ContextData, path: String): Any? {
+        return if (path != contextData.id) {
+            findAndCacheValue(contextData, path)
+        } else {
+            contextData.value
+        }
+    }
+
+    private fun findAndCacheValue(contextData: ContextData, path: String): Any? {
+        return try {
+            val keys = contextPathResolver.getKeysFromPath(contextData.id, path)
+            val foundValue = jsonPathFinder.find(keys, contextData.value)
+            foundValue
+        } catch (ex: Exception) {
+            BeagleMessageLogs.errorWhileTryingToAccessContext(ex)
+            null
         }
     }
 
--- android/beagle/src/test/java/br/com/zup/beagle/android/utils/StringExtensionsKtTest.kt
+++ android/beagle/src/test/java/br/com/zup/beagle/android/utils/StringExtensionsKtTest.kt
@@ -16,118 +16,63 @@
 
 package br.com.zup.beagle.android.utils
 
-import android.graphics.Color
-import br.com.zup.beagle.android.utils.toAndroidColor
-import io.mockk.every
-import io.mockk.mockkStatic
-import io.mockk.slot
-import org.junit.Before
+import br.com.zup.beagle.android.utils.getExpressions
 import org.junit.Test
 import kotlin.test.assertEquals
 
 class StringExtensionsKtTest {
 
-    private val colorSlot = slot<String>()
+    @Test
+    fun getExpressions_should_return_one_expression_present_on_string() {
+        // Given
+        val text = "@{exp1}"
 
-    @Before
-    fun setUp() {
-        mockkStatic(Color::class)
-        every { Color.parseColor(capture(colorSlot)) } returns 0
+        // When
+        val expressions = text.getExpressions()
+
+        // Then
+        assertEquals(1, expressions.size)
+        assertEquals("exp1", expressions[0])
     }
 
     @Test
-    fun toHexColor_should_return_00BB22_when_0B2_is_given() {
+    fun getExpressions_should_return_one_expression_present_on_text_string() {
         // Given
-        val rgb = "#0B2"
+        val text = "I have this @{exp1}"
 
         // When
-        rgb.toAndroidColor()
+        val expressions = text.getExpressions()
 
         // Then
-        assertEquals("#00BB22", colorSlot.captured)
+        assertEquals(1, expressions.size)
+        assertEquals("exp1", expressions[0])
     }
 
     @Test
-    fun toHexColor_should_return_aa55cc_when_a5c_is_given() {
+    fun getExpressions_should_return_two_expressions_present_on_text_string() {
         // Given
-        val rgb = "#a5c"
+        val text = "I have this @{exp1} and this one @{exp2}"
 
         // When
-        rgb.toAndroidColor()
+        val expressions = text.getExpressions()
 
         // Then
-        assertEquals("#aa55cc", colorSlot.captured)
+        assertEquals(2, expressions.size)
+        assertEquals("exp1", expressions[0])
+        assertEquals("exp2", expressions[1])
     }
 
     @Test
-    fun toHexColor_should_return_AA00BB22_when_0B2A_is_given() {
+    fun getExpressions_should_return_two_expressions_present_on_beginning_of_text_string() {
         // Given
-        val rgba = "#0B2A"
+        val text = "@{exp1} and this one @{exp2}"
 
         // When
-        rgba.toAndroidColor()
+        val expressions = text.getExpressions()
 
         // Then
-        assertEquals("#AA00BB22", colorSlot.captured)
-    }
-
-    @Test
-    fun toHexColor_should_return_11bb44dd_when_1b4d_is_given() {
-        // Given
-        val rgba = "#1b4d"
-
-        // When
-        rgba.toAndroidColor()
-
-        // Then
-        assertEquals("#dd11bb44", colorSlot.captured)
-    }
-
-    @Test
-    fun toHexColor_should_return_A5C321_when_A5C321_is_given() {
-        // Given
-        val rgb = "#A5C321"
-
-        // When
-        rgb.toAndroidColor()
-
-        // Then
-        assertEquals("#A5C321", colorSlot.captured)
-    }
-
-    @Test
-    fun toHexColor_should_return_ffffff_when_ffffff_is_given() {
-        // Given
-        val rgb = "#ffffff"
-
-        // When
-        rgb.toAndroidColor()
-
-        // Then
-        assertEquals("#ffffff", colorSlot.captured)
-    }
-
-    @Test
-    fun toHexColor_should_return_5042FD7A_when_42FD7A50_is_given() {
-        // Given
-        val rgb = "#42FD7A50"
-
-        // When
-        rgb.toAndroidColor()
-
-        // Then
-        assertEquals("#5042FD7A", colorSlot.captured)
-    }
-
-    @Test
-    fun toHexColor_should_return_60A218DC_when_A218DC60_is_given() {
-        // Given
-        val rgb = "#a218dc60"
-
-        // When
-        rgb.toAndroidColor()
-
-        // Then
-        assertEquals("#60a218dc", colorSlot.captured)
+        assertEquals(2, expressions.size)
+        assertEquals("exp1", expressions[0])
+        assertEquals("exp2", expressions[1])
     }
 }
