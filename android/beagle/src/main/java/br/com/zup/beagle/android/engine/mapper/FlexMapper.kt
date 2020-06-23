bf8e483c91f3256718c9fec9d91169fa1bc16138
Hernand Azevedo <hernand.azevedo@zup.com.br>
2020-06-23 17:22:32 -0300

    fix: :bug: Fixes flex shrink that causes pageindicator bug, also other flex bugs due to default values being ignored (#345)
    Signed-off-by: Hernand Azevedo <hernand.azevedo@zup.com.br>

--- android/beagle/src/main/java/br/com/zup/beagle/android/engine/mapper/FlexMapper.kt
+++ android/beagle/src/main/java/br/com/zup/beagle/android/engine/mapper/FlexMapper.kt
@@ -41,9 +41,9 @@ class FlexMapper {
         alignItems = makeYogaAlignItems(style.flex?.alignItems) ?: YogaAlign.STRETCH
         alignSelf = makeYogaAlignSelf(style.flex?.alignSelf) ?: YogaAlign.AUTO
         alignContent = makeYogaAlignContent(style.flex?.alignContent) ?: YogaAlign.FLEX_START
-        style.flex?.grow?.toFloat()?.let { flexGrow = it }
-        style.flex?.flex?.toFloat()?.let { setFlex(it) }
-        style.flex?.shrink?.toFloat()?.let { flexShrink = it }
+        flexGrow = style.flex?.grow?.toFloat() ?: 0.0f
+        flexShrink = style.flex?.shrink?.toFloat() ?: 1.0f
+        flex = style.flex?.flex?.toFloat() ?: 0.0f
         display = makeYogaDisplay(style.display) ?: YogaDisplay.FLEX
         positionType = makeYogaPositionType(style.positionType) ?: YogaPositionType.RELATIVE
         applyAttributes(style, this)
