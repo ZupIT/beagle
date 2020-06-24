87cefe3a38cf8e79125050687510bb3004d24797
Vitor Basso <vitor.basso@zup.com.br>
2020-06-24 15:44:13 -0300

    open class
    opened class and functions

--- backend/sample/spring/src/main/kotlin/br/com/zup/beagle/sample/spring/config/CorsConfig.kt
+++ backend/sample/spring/src/main/kotlin/br/com/zup/beagle/sample/spring/config/CorsConfig.kt
@@ -22,10 +22,10 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
 @Configuration
-class CorsConfig {
+open class CorsConfig {
 
     @Bean
-    fun corsConfigurer(): WebMvcConfigurer? {
+    open fun corsConfigurer(): WebMvcConfigurer? {
         return object : WebMvcConfigurer {
             override fun addCorsMappings(registry: CorsRegistry) {
                 registry
