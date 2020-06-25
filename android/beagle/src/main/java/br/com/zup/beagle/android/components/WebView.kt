a2e3c561548d6415456eb6e4d3eee38d8f9e85f2
Uzias Ferreira <63263091+uziasferreirazup@users.noreply.github.com>
2020-06-25 00:54:15 -0300

    fix: add support to javascript in webview (#374)

--- android/beagle/src/main/java/br/com/zup/beagle/android/components/WebView.kt
+++ android/beagle/src/main/java/br/com/zup/beagle/android/components/WebView.kt
@@ -16,6 +16,7 @@
 
 package br.com.zup.beagle.android.components
 
+import android.annotation.SuppressLint
 import android.content.Context
 import android.graphics.Bitmap
 import android.net.http.SslError
@@ -41,9 +42,11 @@ data class WebView(
     @Transient
     private val viewFactory = ViewFactory()
 
+    @SuppressLint("SetJavaScriptEnabled")
     override fun buildView(rootView: RootView): View {
         val webView = viewFactory.makeWebView(rootView.getContext())
         webView.webViewClient = BeagleWebViewClient(webView.context)
+        webView.settings.javaScriptEnabled = true
         observeBindChanges(rootView, url) {
             webView.loadUrl(it)
         }
