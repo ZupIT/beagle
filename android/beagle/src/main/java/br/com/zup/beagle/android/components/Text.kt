0570641d1f216b931569695de834a575a3b0c842
Jeferson Lopes <66736271+jefersonlopeszup@users.noreply.github.com>
2020-06-29 18:23:36 -0300

    fix: adjust in TextView for not ignoring attributes (#371)
    * add style in construction of TextView and Button

--- android/beagle/src/main/java/br/com/zup/beagle/android/components/Text.kt
+++ android/beagle/src/main/java/br/com/zup/beagle/android/components/Text.kt
@@ -16,6 +16,7 @@
 
 package br.com.zup.beagle.android.components
 
+import android.os.Build
 import android.view.Gravity
 import android.view.View
 import android.widget.TextView
@@ -53,7 +54,10 @@ data class Text(
     private val viewFactory = ViewFactory()
 
     override fun buildView(rootView: RootView): View {
-        val textView = viewFactory.makeTextView(rootView.getContext())
+        val textView = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP)
+            viewFactory.makeTextView(rootView.getContext(), getStyleId(this.styleId))
+            else viewFactory.makeTextView(rootView.getContext())
+
         textView.setTextWidget(this, rootView)
         return textView
     }
@@ -94,6 +98,9 @@ data class Text(
             TextViewCompat.setTextAppearance(this, it)
         }
     }
+
+    private fun getStyleId(styleName: String?) : Int =
+            BeagleEnvironment.beagleSdk.designSystem?.textStyle(styleName ?:"")?:0
 
     private fun TextView.setTextColor(color: String?) {
         color?.let {
