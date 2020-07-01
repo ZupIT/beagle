40482c59f331a172d8a986ca5baa8e7d26feb8f5
Vinicius Guardieiro Sousa <vinicius.guardieiro@zup.com.br>
2020-07-01 15:26:29 -0300

    import fragment in some classes

--- android/sample/src/main/java/br/com/zup/beagle/sample/fragment/ImageViewFragment.kt
+++ android/sample/src/main/java/br/com/zup/beagle/sample/fragment/ImageViewFragment.kt
@@ -17,6 +17,7 @@
 package br.com.zup.beagle.sample.fragment
 
 import android.os.Bundle
+import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
