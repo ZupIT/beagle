8a30959fe72b28c84a09097fa633d0be0779b7ac
Lucas Sousa Silva <lucas.silva@zup.com.br>
2020-07-20 17:25:20 -0300

    Adding some comments to backend automated tests deploy script

--- fastlane/automatedTests/deploy_tests_backend.sh
+++ fastlane/automatedTests/deploy_tests_backend.sh
@@ -4,7 +4,8 @@ cd ../../backend/automated-tests
 #./gradlew assemble 
 IMAGE_ID=$(docker build . 2>/dev/null | awk '/Successfully built/{print $NF}')
 echo $IMAGE_ID
+#important! -> a ordem dos parâmetros importa, o -p 8080:8080 deve ser antes do IMAGE_ID
 docker run -p 8080:8080 $IMAGE_ID
-#is it necessary?
+#returns the mac machine ip -> is it necessary?
 #SERVER_IP=$(ipconfig getifaddr en0)
 #echo $SERVER_IP
