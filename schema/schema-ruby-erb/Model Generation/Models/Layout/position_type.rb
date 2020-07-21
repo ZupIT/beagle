aa36da27df515e994c3d4fcb9932f28fa59b26f9
Luiz Gustavo Gomes Borges <luiz.borges@zup.com.br>
2020-07-21 10:56:53 -0300

    Adjusted relative imports.
    Signed-off-by: Luiz Gustavo Gomes Borges <luiz.borges@zup.com.br>

--- schema/schema-ruby-erb/Model Generation/Models/Layout/position_type.rb
+++ schema/schema-ruby-erb/Model Generation/Models/Layout/position_type.rb
@@ -12,7 +12,7 @@
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
 
-require_relative '../../Synthax/Variable'
+require_relative '../../Synthax/variable.rb'
 require_relative '../base_component.rb'
 
 class PositionType < BaseComponent
