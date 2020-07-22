0afe67b3bf9ad654c34e3e2e2aabd23ece4e7bf1
Vinicius Guardieiro Sousa <vinicius.guardieiro@zup.com.br>
2020-07-22 09:58:20 -0300

    change constant place and change the pageView constructor to private

--- backend/widgets/src/main/kotlin/br/com/zup/beagle/widget/layout/PageView.kt
+++ backend/widgets/src/main/kotlin/br/com/zup/beagle/widget/layout/PageView.kt
@@ -21,8 +21,8 @@ import br.com.zup.beagle.widget.action.Action
 import br.com.zup.beagle.widget.context.Bind
 import br.com.zup.beagle.widget.context.ContextComponent
 import br.com.zup.beagle.widget.context.ContextData
-import br.com.zup.beagle.widget.pager.DEPRECATED_PAGE_VIEW
 import br.com.zup.beagle.widget.pager.PageIndicatorComponent
+import br.com.zup.beagle.widget.utils.BeagleConstants.DEPRECATED_PAGE_VIEW
 
 /**
  *  The PageView component is a specialized container to hold pages (views) that will be displayed horizontally.
@@ -32,7 +32,7 @@ import br.com.zup.beagle.widget.pager.PageIndicatorComponent
  *
  */
 
-data class PageView(
+private data class PageView(
     val children: List<ServerDrivenComponent>,
     @Deprecated(DEPRECATED_PAGE_VIEW)
     val pageIndicator: PageIndicatorComponent? = null,
