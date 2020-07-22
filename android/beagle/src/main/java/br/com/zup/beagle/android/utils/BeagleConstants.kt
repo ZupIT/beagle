8cab04107eb9c608bfacf8b32931ccb81f42717f
Vinicius Guardieiro Sousa <vinicius.guardieiro@zup.com.br>
2020-07-22 09:44:57 -0300

    solve detekt

--- android/beagle/src/main/java/br/com/zup/beagle/android/utils/BeagleConstants.kt
+++ android/beagle/src/main/java/br/com/zup/beagle/android/utils/BeagleConstants.kt
@@ -18,5 +18,6 @@ package br.com.zup.beagle.android.utils
 
 object BeagleConstants {
     val EXPRESSION_REGEX = "@\\{([^)]+)\\}".toRegex()
-    const val DEPRECATED_PAGE_VIEW = "This constructor will be removed in a future version, use the constructor with Bind"
+    const val DEPRECATED_PAGE_VIEW =
+        "This constructor will be removed in a future version, use the constructor with Bind"
 }
