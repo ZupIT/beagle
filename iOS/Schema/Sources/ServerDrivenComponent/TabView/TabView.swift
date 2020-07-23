54bcf910b4aaf4e9f92abdef5b6cb9c565e78a61
Pocas <yan.dias@zup.com.br>
2020-07-23 11:48:58 -0300

    Add deprecated message

--- iOS/Schema/Sources/ServerDrivenComponent/TabView/TabView.swift
+++ iOS/Schema/Sources/ServerDrivenComponent/TabView/TabView.swift
@@ -57,6 +57,7 @@ public struct TabItem: Decodable {
     }
 }
 
+@available(*, deprecated, message: "Since version 1.1. Will be deleted in version 2.0. Consider replacing this component for a tabBar with a pageview.")
 public struct TabView: RawComponent, AutoInitiable, HasContext {
     public let children: [TabItem]
     public let styleId: String?
