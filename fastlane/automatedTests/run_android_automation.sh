46c6407b8891a8e86f6ae96a31585b22121e8d50
José Augusto <ze12augusto@gmail.com>
2020-07-23 15:39:42 -0300

    Add logic to check if the emulator is up
    Signed-off-by: José Augusto <ze12augusto@gmail.com>

--- fastlane/automatedTests/run_android_automation.sh
+++ fastlane/automatedTests/run_android_automation.sh
@@ -43,4 +43,4 @@ ENDSCRIPT
 echo "Waiting 30 secs for us to be really booted"
 sleep 30
 
-./gradlew -p android/automated-tests connectedAndroidTest
+#./gradlew -p android/automated-tests connectedAndroidTest
