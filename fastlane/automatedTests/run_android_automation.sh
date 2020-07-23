7b4ddd5cbf0a789c0956ba56495cc9aa37d1c1a8
Luiz Gustavo Gomes Borges <luiz.borges@zup.com.br>
2020-07-22 21:14:22 -0300

    Configured Android automated tests.
    Signed-off-by: Luiz Gustavo Gomes Borges <luiz.borges@zup.com.br>

--- fastlane/automatedTests/run_android_automation.sh
+++ fastlane/automatedTests/run_android_automation.sh
@@ -1,3 +1,27 @@
 #!/bin/bash
 
-"$ANDROID_SDK_ROOT"/tools/bin/avdmanager create avd -n test -k "system-images;android-29;google_apis_playstore;x86"
+AVD_NAME='test'
+EMULATOR_PID=''
+
+function cleanup() {
+    if [[ -n $EMULATOR_PID ]]; then
+        kill $EMULATOR_PID
+    fi
+
+    "$ANDROID_SDK_ROOT"/tools/bin/avdmanager delete avd -n $AVD_NAME
+}
+
+trap exit SIGHUP SIGINT
+trap cleanup EXIT
+
+#blank line necessary as input to AVD
+"$ANDROID_SDK_ROOT"/tools/bin/avdmanager create avd -n $AVD_NAME -k "system-images;android-29;google_apis_playstore;x86" <<EOF
+
+EOF
+
+nohup "$ANDROID_SDK_ROOT"/emulator/emulator -avd test -no-audio -no-boot-anim -no-snapshot -no-window 2>&1 &
+EMULATOR_PID=$!
+
+"$ANDROID_SDK_ROOT"/platform-tools/adb wait-for-device
+
+./gradlew -p android/automated-tests connectedAndroidTest
