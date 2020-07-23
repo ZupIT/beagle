b38e10e18290c973546aee9a624eee5237113af8
José Augusto <ze12augusto@gmail.com>
2020-07-23 16:50:42 -0300

    Add logic to check if the emulator is up
    Signed-off-by: José Augusto <ze12augusto@gmail.com>

--- fastlane/automatedTests/run_android_automation.sh
+++ fastlane/automatedTests/run_android_automation.sh
@@ -4,11 +4,7 @@ AVD_NAME='test'
 AVD_IMAGE='system-images;android-29;google_apis_playstore;x86'
 
 function cleanup() {
-    if [[ -n $EMULATOR_PID ]]; then
-        kill "$EMULATOR_PID"
-    fi
-
-    "$ANDROID_SDK_ROOT"/tools/bin/avdmanager delete avd -n $AVD_NAME
+    "$ANDROID_SDK_ROOT"/platform-tools/adb devices | grep emulator | cut -f1 | while read line; do adb -s $line emu kill; done
 }
 
 trap exit SIGHUP SIGINT
@@ -16,13 +12,16 @@ trap cleanup EXIT
 
 "$ANDROID_SDK_ROOT"/tools/bin/sdkmanager "$AVD_IMAGE"
 
-#blank line necessary as input to AVD
-"$ANDROID_SDK_ROOT"/tools/bin/avdmanager create avd -n $AVD_NAME -k "$AVD_IMAGE" <<EOF
+if [[ -n $("$ANDROID_SDK_ROOT"/emulator/emulator -list-avds | grep -q "$AVD_NAME") ]]; then
+    echo "Using avd from cache"
+else
+    #blank line necessary as input to AVD
+    "$ANDROID_SDK_ROOT"/tools/bin/avdmanager create avd -n $AVD_NAME -k "$AVD_IMAGE" <<EOF
 
 EOF
+fi
 
-nohup "$ANDROID_SDK_ROOT"/emulator/emulator -avd $AVD_NAME -no-audio -no-boot-anim -no-snapshot -no-window 2>&1 &
-EMULATOR_PID=$!
+nohup "$ANDROID_SDK_ROOT"/emulator/emulator -avd $AVD_NAME -no-audio -no-boot-anim -no-window 2>&1 &
 
 "$ANDROID_SDK_ROOT"/platform-tools/adb wait-for-device shell <<ENDSCRIPT
 echo -n "Waiting for device to boot "
