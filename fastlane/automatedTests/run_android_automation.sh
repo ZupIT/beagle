ee229ffdcc01b1b77c180c3efda3b3e67daa69ce
Luiz Gustavo Gomes Borges <luiz.borges@zup.com.br>
2020-07-24 14:11:43 -0300

    Set custom resolution on Android test emulator.
    Signed-off-by: Luiz Gustavo Gomes Borges <luiz.borges@zup.com.br>

--- fastlane/automatedTests/run_android_automation.sh
+++ fastlane/automatedTests/run_android_automation.sh
@@ -18,12 +18,10 @@ if "$ANDROID_SDK_ROOT"/emulator/emulator -list-avds | grep -q "$AVD_NAME"; then
     echo "Using avd from cache"
 else
     #blank line necessary as input to AVD
-    "$ANDROID_SDK_ROOT"/tools/bin/avdmanager create avd -n $AVD_NAME -k "$AVD_IMAGE" <<EOF
-
-EOF
+    echo | "$ANDROID_SDK_ROOT"/tools/bin/avdmanager create avd -n $AVD_NAME -k "$AVD_IMAGE"
 fi
 
-nohup "$ANDROID_SDK_ROOT"/emulator/emulator -avd $AVD_NAME -no-audio -no-boot-anim -no-window 2>&1 &
+nohup "$ANDROID_SDK_ROOT"/emulator/emulator -avd $AVD_NAME -no-audio -no-boot-anim -no-window -skin 450x800 2>&1 &
 
 "$ANDROID_SDK_ROOT"/platform-tools/adb wait-for-device shell <<ENDSCRIPT
 echo -n "Waiting for device to boot "
