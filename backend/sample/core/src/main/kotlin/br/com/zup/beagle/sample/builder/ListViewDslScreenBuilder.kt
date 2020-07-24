e872863fef37bf5d4d106584a1283f8d30f86e62
Vitor Basso <vitor.basso@zup.com.br>
2020-07-24 11:02:48 -0300

    sample dsl
    A first sample for dsl, building the ListViewScreenBuilder using the dsl builders

---
+++ backend/sample/core/src/main/kotlin/br/com/zup/beagle/sample/builder/ListViewDslScreenBuilder.kt
@@ -0,0 +1,106 @@
+/*
+ * Copyright 2020 ZUP IT SERVICOS EM TECNOLOGIA E INOVACAO SA
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package br.com.zup.beagle.sample.builder
+
+import br.com.zup.beagle.core.Style
+import br.com.zup.beagle.ext.applyStyle
+import br.com.zup.beagle.ext.unitReal
+import br.com.zup.beagle.widget.action.alert
+import br.com.zup.beagle.widget.core.EdgeValue
+import br.com.zup.beagle.widget.core.ListDirection
+import br.com.zup.beagle.widget.core.ScrollAxis
+import br.com.zup.beagle.widget.layout.ScreenBuilder
+import br.com.zup.beagle.widget.layout.container
+import br.com.zup.beagle.widget.layout.navigationBarItem
+import br.com.zup.beagle.widget.layout.screen
+import br.com.zup.beagle.widget.layout.scrollView
+import br.com.zup.beagle.widget.ui.listView
+import br.com.zup.beagle.widget.ui.text
+
+object ListViewDslScreenBuilder : ScreenBuilder {
+    override fun build() = screen{
+        navigationBar {
+            title { "Beagle ListView" }
+            showBackButton{true}
+            navigationBarItems {
+                +navigationBarItem{
+                    text{""}
+                    image{
+                        mobileId{"informationImage"}
+                    }
+                    action{
+                        alert{
+                            title{-"ListView"}
+                            message{-("Is a Layout component that will define a list of views natively. " +
+                                "These views could be any Server Driven Component.")}
+                            labelOk{"OK"}
+                        }
+                    }
+                }
+            }
+        }
+        child{
+            scrollView {
+                scrollDirection{ScrollAxis.VERTICAL}
+                children{
+                    +getStaticListView(ListDirection.VERTICAL)
+                    +getStaticListView(ListDirection.HORIZONTAL)
+                    +getDynamicListView(ListDirection.VERTICAL)
+                    +getDynamicListView(ListDirection.HORIZONTAL)
+                }
+            }
+        }
+    }
+
+    private fun getStaticListView(listDirection: ListDirection) = container {
+        children{
+            +text{
+                text{-"Static $listDirection ListView"}
+            }.applyStyle(Style(
+                margin = EdgeValue(bottom = 10.unitReal())
+            ))
+            +listView {
+                direction{listDirection}
+                children {
+                    +(1..10).map(::createText)
+                }
+            }
+        }
+    }.applyStyle(Style(
+        margin = EdgeValue(bottom = 20.unitReal())
+    ))
+
+    private fun getDynamicListView(listDirection: ListDirection) = container{
+        children{
+            +text {
+                text{-"Dynamic $listDirection ListView"}
+            }.applyStyle(Style(
+                margin = EdgeValue(bottom = 10.unitReal())
+            ))
+            +listView {
+                direction { listDirection }
+                children {
+                    +(0 until 20).map(::createText)
+                }
+            }
+        }
+    }
+
+    private fun createText(index: Int) = text{
+        text{-"Hello $index"}
+    }
+}
---
+++ backend/sample/micronaut/src/main/kotlin/br/com/zup/beagle/sample/micronaut/controller/DslController.kt
@@ -0,0 +1,30 @@
+/*
+ * Copyright 2020 ZUP IT SERVICOS EM TECNOLOGIA E INOVACAO SA
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package br.com.zup.beagle.sample.micronaut.controller
+
+import br.com.zup.beagle.sample.constants.DSL_BUILDER_ENDPOINT
+import br.com.zup.beagle.sample.micronaut.service.SampleDslService
+import io.micronaut.http.annotation.Controller
+import io.micronaut.http.annotation.Get
+
+@Controller
+class DslController(
+    private val dslBuilderService: SampleDslService
+) {
+    @Get(DSL_BUILDER_ENDPOINT)
+    fun getDslBuiltScreen() = this.dslBuilderService.getDslScreen()
+}
