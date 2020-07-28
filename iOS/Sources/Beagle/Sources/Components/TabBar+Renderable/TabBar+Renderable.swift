c4ffee9f8ef234bf4ad9006c4f76468aed7f81b5
Yan Lucas <42281214+yandiaszup@users.noreply.github.com>
2020-07-28 11:03:17 -0300

    feat: TabBar Componnent (#620)
    * Create folders, files, and renaming TabView files
    
    * Add TabBar component
    
    * TabViewUIComponent now uses TabBarUIComponent as TabBar
    
    * Add binding attributes to TabBar
    
    * Adjust PageView theme
    
    * Add sample
    
    * Corrections to TabBarUIComponent
    
    * Refactor PageViewTests
    
    * Adding some tests to TabBar
    
    * Fix tabBar bug when scrolling to nom visible cells
    
    * Update tests
    
    * Update TabBarItem decoder init and TabBar test
    
    * Add deprecated message
    
    * Rename TabBar children property to items
    
    * Removing some tests

---
+++ iOS/Sources/Beagle/Sources/Components/TabBar+Renderable/TabBar+Renderable.swift
@@ -0,0 +1,38 @@
+//
+/*
+ * Copyright 2020 ZUP IT SERVICOS EM TECNOLOGIA E INOVACAO SA
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import UIKit
+import BeagleSchema
+
+extension TabBar: ServerDrivenComponent {
+    public func toView(renderer: BeagleRenderer) -> UIView {
+        let view = TabBarUIComponent(model: .init(tabIndex: 0, tabBarItems: items))
+        
+        if let currentTab = currentTab {
+            renderer.observe(currentTab, andUpdateManyIn: view) { tab in
+                view.scrollTo(page: tab)
+            }
+        }
+        
+        view.onTabSelection = { tab in
+            let context = Context(id: "onTabSelection", value: .int(tab))
+            renderer.controller.execute(actions: self.onTabSelection, with: context, sender: view)
+        }
+        
+        return view
+    }
+}
