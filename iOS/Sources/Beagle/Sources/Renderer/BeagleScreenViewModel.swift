140e5b650192342517757683f137eb938bef2b40
Lucas Araújo <lucasaraujo@users.noreply.github.com>
2020-07-29 09:51:01 -0300

    fix: context in PageView and LazyComponent (#606)
    * fix context in PageView and LazyComponent
    
    Signed-off-by: Lucas Araújo <lucas.araujo@zup.com.br>
    
    * remove empty test
    
    Signed-off-by: Lucas Araújo <lucas.araujo@zup.com.br>

--- iOS/Sources/Beagle/Sources/Renderer/BeagleScreenViewModel.swift
+++ iOS/Sources/Beagle/Sources/Renderer/BeagleScreenViewModel.swift
@@ -20,7 +20,10 @@ import BeagleSchema
 class BeagleScreenViewModel {
         
     var screenType: ScreenType {
-        didSet { screen = nil }
+        didSet {
+            screenAppearEventIsPending = true
+            screen = nil
+        }
     }
     var screen: Screen?
     var state: State {
@@ -35,6 +38,8 @@ class BeagleScreenViewModel {
     }
 
     var dependencies: BeagleDependenciesProtocol
+    
+    private var screenAppearEventIsPending = true
 
     // MARK: Observer
 
@@ -88,6 +93,19 @@ class BeagleScreenViewModel {
             tryToLoadScreenFromText(text)
         }
     }
+    
+    public func trackEventOnScreenAppeared() {
+        if let event = screen?.screenAnalyticsEvent {
+            screenAppearEventIsPending = false
+            dependencies.analytics?.trackEventOnScreenAppeared(event)
+        }
+    }
+    
+    public func trackEventOnScreenDisappeared() {
+        if let event = screen?.screenAnalyticsEvent {
+            dependencies.analytics?.trackEventOnScreenDisappeared(event)
+        }
+    }
 
     // MARK: Core
     
@@ -116,6 +134,9 @@ class BeagleScreenViewModel {
             switch result {
             case .success(let screen):
                 self.handleRemoteScreenSuccess(screen)
+                if self.screenAppearEventIsPending {
+                    self.trackEventOnScreenAppeared()
+                }
             case .failure(let error):
                 self.handleRemoteScreenFailure(error)
             }
