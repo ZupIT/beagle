4b5a8278234e318c0248cad0577edf6bb5937af9
Luiz Gustavo Gomes Borges <luiz.borges@zup.com.br>
2020-08-03 19:06:31 -0300

    Removed explicit version from published BFF image.
    Signed-off-by: Luiz Gustavo Gomes Borges <luiz.borges@zup.com.br>

--- fastlane/automatedTests/publish_test_backend.sh
+++ fastlane/automatedTests/publish_test_backend.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 BACKEND_DIR=backend
-IMAGE_NAME=$(echo "$registry/$repository/$image:$sha" | tr [:upper:] [:lower:])
+IMAGE_NAME=$(echo "$registry/$repository/$image" | tr [:upper:] [:lower:])
 $BACKEND_DIR/gradlew -p $BACKEND_DIR automated-tests:bootBuildImage --imageName=$IMAGE_NAME
 echo $token | docker login $registry -u $user --password-stdin
 docker push $IMAGE_NAME
