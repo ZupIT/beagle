c1055e45b0aa0758529d74b437ccb24e6c76d6b0
Gabriela Coelho <gabsaac@gmail.com>
2020-08-03 14:43:29 -0300

    Adding test config

---
+++ iOS/Example/CocoaPodsTest/CocoaPodsTestUnitTests/CocoaPodsTestUnitTests.swift
@@ -0,0 +1,32 @@
+//
+//  CocoaPodsTestUnitTests.swift
+//  CocoaPodsTestUnitTests
+//
+//  Created by Gabriela Coelho on 03/08/20.
+//
+
+import XCTest
+
+class CocoaPodsTestUnitTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
