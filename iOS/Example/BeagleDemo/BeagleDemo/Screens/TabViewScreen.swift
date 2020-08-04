40e98619dc01b390b937766fe75945dc244acf21
Pocas <yan.dias@zup.com.br>
2020-08-04 16:42:29 -0300

    Refactoring TabBarUIComponnent collection view layout

--- iOS/Example/BeagleDemo/BeagleDemo/Screens/TabViewScreen.swift
+++ iOS/Example/BeagleDemo/BeagleDemo/Screens/TabViewScreen.swift
@@ -28,60 +28,41 @@ struct TabViewScreen: DeeplinkScreen {
     
     var screen =
         Screen(navigationBar: NavigationBar(title: "TabView")) {
-            TabView {
-                TabItem(icon: "beagle") {
+            Container(context: Context(id: "currentTab", value: 2), widgetProperties: .init(Flex().grow(1))) {
+                TabBar(
+                    items: [
+                        TabBarItem(icon: "beagle"),
+                        TabBarItem(title: "Tab 1"),
+                        TabBarItem(title: "Tab 2"),
+                        TabBarItem(icon: "beagle", title: "Tab 3")
+                    ],
+                    currentTab: "@{currentTab}",
+                    onTabSelection: [SetContext(contextId: "currentTab", value: "@{onTabSelection}")]
+                )
+                PageView(
+                    onPageChange: [SetContext(contextId: "currentTab", value: "@{onPageChange}")],
+                    currentPage: "@{currentTab}"
+                ) {
                     Container(widgetProperties: .init(Flex().alignContent(.center))) {
-                        Text("Blaaslkdjfaskldjfalskdjfasldjfasldfj")
+                        Text("Text1 Tab 0")
                         Image(.value(.remote(.init(url: .NETWORK_IMAGE_BEAGLE, placeholder: "imageBeagle"))))
-                        Text("Blaaslkdjfaskldjfalskdjfasldjfasldfj")
+                        Text("Text2 Tab 0")
                     }
-                }
-                
-                TabItem(title: "Tab 2 com titulo") {
                     Container(widgetProperties: .init(Flex().justifyContent(.center).alignItems(.center))) {
+                        Text("Text1 Tab 1")
+                        Text("Text2 Tab 1")
+                    }
+                    Container(widgetProperties: .init(Flex().justifyContent(.flexStart))) {
                         Text("Text1 Tab 2")
                         Text("Text2 Tab 2")
                     }
-                }
-                
-                TabItem(title: "Tab 3") {
-                    Container(widgetProperties: .init(Flex().justifyContent(.flexStart))) {
+                    Container(widgetProperties: .init(Flex().alignItems(.center))) {
                         Text("Text1 Tab 3")
                         Text("Text2 Tab 3")
                     }
                 }
-                
-                TabItem(icon: "beagle", title: "Tab 4") {
-                    Container(widgetProperties: .init(Flex().alignItems(.center))) {
-                        Text("Text1 Tab 4")
-                        Text("Text2 Tab 4")
-                    }
-                }
-                
-                
-                TabItem(icon: "beagle", title: "Tab with Tab view") {
-                    Container(widgetProperties: .init(
-                                style: .init(size: Size().width(400),
-                                             flex: Flex().alignSelf(.center).alignItems(.center).grow(1)))) {
-                            TabView {
-                                TabItem(title: "Sub tab 1") {
-                                    Container(widgetProperties: .init(Flex().alignItems(.center).grow(1))) {
-                                        Text("Subtext 1 Tab 1")
-                                        Text("Subtext 2 Tab 1")
-                                    }
-                                }
-                                TabItem(title: "Sub tab 2") {
-                                    Container(widgetProperties: .init(Flex().alignItems(.center).grow(1))) {
-                                        Text("Subtext 1 Tab 2")
-                                        Text("Subtext 2 Tab 2")
-                                    }
-                                }
-
-                            }
-                        }
-                    }
             }
-    }
+        }
 }
 
 
