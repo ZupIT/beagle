5b868749fde90c3d66874ad1cb0f1454a18d4250
Diogo <diogo.oliveira@zup.com.br>
2020-08-06 01:52:33 -0300

    Code Review: Constants name instead unicode

--- android/beagle/src/main/java/br/com/zup/beagle/android/context/operations/grammar/GrammarChars.kt
+++ android/beagle/src/main/java/br/com/zup/beagle/android/context/operations/grammar/GrammarChars.kt
@@ -17,13 +17,13 @@
 package br.com.zup.beagle.android.context.operations.grammar
 
 internal object GrammarChars {
-    const val OPEN_PARENTHESES = '\u0028'
-    const val CLOSE_PARENTHESES = '\u0029'
-    const val OPEN_BRACES = '\u007b'
-    const val CLOSE_BRACES = '\u007d'
-    const val OPEN_BRACKET = '\u005b'
-    const val CLOSE_BRACKET = '\u005d'
-    const val COMMA = '\u002c'
+    const val OPEN_PARENTHESES = '('
+    const val CLOSE_PARENTHESES = ')'
+    const val OPEN_BRACES = '{'
+    const val CLOSE_BRACES = '}'
+    const val OPEN_BRACKET = '['
+    const val CLOSE_BRACKET = ']'
+    const val COMMA = ','
     const val APOSTROPHE_MARK = "'"
     const val BOOLEAN_VALUE_TRUE = "true"
     const val BOOLEAN_VALUE_FALSE = "false"
