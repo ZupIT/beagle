682fee95c71f9a68330c78ffb99a66e16ef26c12
Hernand Azevedo <hernand.azevedo@zup.com.br>
2020-08-20 17:08:31 -0300

    :bug: Adds method to check if the activity have a server driven screen loaded
    Signed-off-by: Hernand Azevedo <hernand.azevedo@zup.com.br>

--- android/beagle/src/main/java/br/com/zup/beagle/android/view/BeagleActivity.kt
+++ android/beagle/src/main/java/br/com/zup/beagle/android/view/BeagleActivity.kt
@@ -158,6 +158,8 @@ abstract class BeagleActivity : AppCompatActivity() {
         }
     }
 
+    fun hasServerDrivenScreen(): Boolean = supportFragmentManager.backStackEntryCount > 0
+
     fun navigateTo(screenRequest: ScreenRequest, screen: Screen?) {
         fetch(screenRequest, screen?.toComponent())
     }
