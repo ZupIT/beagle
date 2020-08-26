640e4125b0f6225558a2ccef2c9672e2b389d6b8
Vinicius Guardieiro Sousa <vinicius.guardieiro@zup.com.br>
2020-08-26 16:29:48 -0300

    revert text input

--- android/sample/src/main/java/br/com/zup/beagle/sample/fragment/TextInputFragment.kt
+++ android/sample/src/main/java/br/com/zup/beagle/sample/fragment/TextInputFragment.kt
@@ -67,12 +67,6 @@ class TextInputFragment : Fragment() {
         return declarative.toView(this)
     }
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-        //inflar a main
-        //loadView no endpoint
-
-    }
     companion object {
         fun newInstance(): TextInputFragment {
             return TextInputFragment()
