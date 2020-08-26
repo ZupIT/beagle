885be087ea97ef9ee5477b629110e0e7d45fdf8e
Túlio Magalhães <tuliohdvp@gmail.com>
2020-08-26 00:44:49 -0300

    Rollback changes

--- backend/sample/core/src/main/kotlin/br/com/zup/beagle/sample/builder/ComponentScreenBuilder.kt
+++ backend/sample/core/src/main/kotlin/br/com/zup/beagle/sample/builder/ComponentScreenBuilder.kt
@@ -48,14 +48,13 @@ import br.com.zup.beagle.sample.constants.SCREEN_TOUCHABLE_ENDPOINT
 import br.com.zup.beagle.sample.constants.SCREEN_WEB_VIEW_ENDPOINT
 import br.com.zup.beagle.widget.action.Navigate
 import br.com.zup.beagle.widget.action.Route
-import br.com.zup.beagle.widget.action.SetContext
-import br.com.zup.beagle.widget.context.ContextData
-import br.com.zup.beagle.widget.context.expressionOf
 import br.com.zup.beagle.widget.core.EdgeValue
 import br.com.zup.beagle.widget.core.ScrollAxis
-import br.com.zup.beagle.widget.layout.*
+import br.com.zup.beagle.widget.layout.NavigationBar
+import br.com.zup.beagle.widget.layout.Screen
+import br.com.zup.beagle.widget.layout.ScreenBuilder
+import br.com.zup.beagle.widget.layout.ScrollView
 import br.com.zup.beagle.widget.ui.Button
-import br.com.zup.beagle.widget.ui.Text
 
 object ComponentScreenBuilder : ScreenBuilder {
     override fun build() = Screen(
@@ -63,12 +62,35 @@ object ComponentScreenBuilder : ScreenBuilder {
             title = "Choose a Component",
             showBackButton = true
         ),
-        child = Container(
-            context = ContextData("context", 1),
+        child = ScrollView(
+            scrollDirection = ScrollAxis.VERTICAL,
             children = listOf(
-                Text(expressionOf("Count: @{context}")),
-                Button(text = "Increment", onPress = listOf(SetContext("context", "@{sum(context, 1.0)}"))),
-                Button(text = "Decrement", onPress = listOf(SetContext("context", "@{subtract(context, 1.0)}")))
+                createMenu("Button", SCREEN_BUTTON_ENDPOINT),
+                createMenu("Text", SCREEN_TEXT_ENDPOINT),
+                createMenu("Local Image", SCREEN_IMAGE_ENDPOINT),
+                createMenu("Remote Image", SCREEN_NETWORK_IMAGE_ENDPOINT),
+                createMenu("Bff Remote Image", SCREEN_BFF_NETWORK_IMAGE_ENDPOINT),
+                createMenu("TabView", SCREEN_TAB_VIEW_ENDPOINT),
+                createMenu("ListView", SCREEN_LIST_VIEW_ENDPOINT),
+                createMenu("ScrollView", SCREEN_SCROLL_VIEW_ENDPOINT),
+                createMenu("PageView", SCREEN_PAGE_VIEW_ENDPOINT),
+                createMenu("Action", SCREEN_ACTION_ENDPOINT),
+                createMenu("ScreenBuilder", SCREEN_BUILDER_ENDPOINT),
+                createMenu("Form", SCREEN_FORM_ENDPOINT),
+                createMenu("LazyComponent", SCREEN_LAZY_COMPONENT_ENDPOINT),
+                createMenu("NavigationBar", SCREEN_NAVIGATION_BAR_ENDPOINT),
+                createMenu("NavigationType", NAVIGATION_TYPE_ENDPOINT),
+                createMenu("Accessibility Screen", ACCESSIBILITY_SCREEN_ENDPOINT),
+                createMenu("Compose Component", SCREEN_COMPOSE_COMPONENT_ENDPOINT),
+                createMenu("Touchable", SCREEN_TOUCHABLE_ENDPOINT),
+                createMenu("Analytics", SCREEN_ANALYTICS_ENDPOINT),
+                createMenu("Web View", SCREEN_WEB_VIEW_ENDPOINT),
+                createMenu("Platform", PLATFORM_SAMPLE_ENDPOINT),
+                createMenu("Custom Platform", CUSTOM_PLATFORM_SAMPLE_ENDPOINT),
+                createMenu("Context", SCREEN_CONTEXT_ENDPOINT),
+                createMenu("Safe Area", SCREEN_SAFE_AREA_ENDPOINT),
+                createMenu("Text Input", SCREEN_TEXT_INPUT_ENDPOINT),
+                createMenu("Simple Form", SCREEN_SIMPLE_FORM_ENDPOINT)
             )
         )
     )
