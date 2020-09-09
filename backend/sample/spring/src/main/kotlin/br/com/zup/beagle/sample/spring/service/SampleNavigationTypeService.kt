ffcc0f64dde60390d7674029ff9e7883e6902527
jenifersantoszup <jenifer.santos@zup.com.br>
2020-09-09 17:15:38 -0300

    Organizing imports
    Signed-off-by: jenifersantoszup <jenifer.santos@zup.com.br>

--- backend/sample/spring/src/main/kotlin/br/com/zup/beagle/sample/spring/service/SampleNavigationTypeService.kt
+++ backend/sample/spring/src/main/kotlin/br/com/zup/beagle/sample/spring/service/SampleNavigationTypeService.kt
@@ -16,12 +16,8 @@
 
 package br.com.zup.beagle.sample.spring.service
 
-import br.com.zup.beagle.widget.action.Navigate
-import br.com.zup.beagle.widget.action.Route
-import br.com.zup.beagle.widget.action.Alert
-import br.com.zup.beagle.core.Style
 import br.com.zup.beagle.core.CornerRadius
-import br.com.zup.beagle.ext.applyFlex
+import br.com.zup.beagle.core.Style
 import br.com.zup.beagle.ext.applyStyle
 import br.com.zup.beagle.ext.unitReal
 import br.com.zup.beagle.sample.constants.BUTTON_STYLE_APPEARANCE
@@ -35,10 +31,12 @@ import br.com.zup.beagle.sample.constants.RED_ORANGE
 import br.com.zup.beagle.sample.constants.REPRESENTATION_NAVIGATION_TYPE_STEP2_ENDPOINT
 import br.com.zup.beagle.sample.constants.REPRESENTATION_NAVIGATION_TYPE_STEP3_ENDPOINT
 import br.com.zup.beagle.sample.constants.REPRESENTATION_PRESENT_ENDPOINT
+import br.com.zup.beagle.widget.action.Alert
+import br.com.zup.beagle.widget.action.Navigate
+import br.com.zup.beagle.widget.action.Route
 import br.com.zup.beagle.widget.context.ContextData
 import br.com.zup.beagle.widget.context.expressionOf
 import br.com.zup.beagle.widget.core.EdgeValue
-import br.com.zup.beagle.widget.core.Flex
 import br.com.zup.beagle.widget.layout.Container
 import br.com.zup.beagle.widget.layout.NavigationBar
 import br.com.zup.beagle.widget.layout.NavigationBarItem
