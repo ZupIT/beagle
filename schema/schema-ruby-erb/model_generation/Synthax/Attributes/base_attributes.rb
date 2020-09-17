d787e4b47f53763a64dd70c84a0805156badee53
Gabriel Marson <gabriel.marson@zup.com.br>
2020-09-17 16:41:03 -0300

    documented base attributes

--- schema/schema-ruby-erb/model_generation/Synthax/Attributes/base_attributes.rb
+++ schema/schema-ruby-erb/model_generation/Synthax/Attributes/base_attributes.rb
@@ -12,10 +12,31 @@
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
 
+# This class is a core class of beagle schema. It should NOT be use directly. If you need to use
+# these attributes, use one of its subclasses.
 class BaseAttributes
 
-    attr_accessor :name, :typeName, :accessor, :defaultValue, :comment
+    # The name of the attribute.
+    # @return [String]
+    attr_accessor :name
+
+    # The type to be displayed on the generated code.
+    # @return [String]
+    attr_accessor :typeName
     
+    # The acessor of the attribute. Default is public
+    # @return [String] 
+    attr_accessor :accessor
+
+    # The default value of an attribute. It can be left in blank.
+    # @return [String]
+    attr_accessor :defaultValue
+    
+    # Comment to be generated to this attributed. It can be left in blank.
+    # @return [String]
+    attr_accessor :comment
+    
+    # Initialize for BaseAttributes
     def initialize(params = {})
         @name = params.fetch(:name, '')
         @typeName = params.fetch(:typeName, '')
