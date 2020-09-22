604319f696f80eaf7d02d574bbd9de1fde03022b
theffc <frederico.calhau@zup.com.br>
2020-09-22 16:29:48 -0300

    fix: multiple expression not working when observing due to cache

--- iOS/Sources/Beagle/Sources/ContextExpression/UIView+Context.swift
+++ iOS/Sources/Beagle/Sources/ContextExpression/UIView+Context.swift
@@ -225,12 +225,11 @@ extension UIView {
     // expression last value cache is used only for multiple expressions binding
     private func evaluateWithCache<T: Decodable>(for expression: SingleExpression, contextId: String? = nil) -> T? {
         switch expression {
-        case let .value(.binding(binding)):
-            if contextId == nil || contextId == binding.context {
-                return evaluate(for: expression)
-            } else {
-                return transform(expressionLastValueMap[expression.rawValue] ?? .empty)
-            }
+        // TODO: expression caching is not working with multiple expressions, so we won't use cache for now, until we
+        // we have a better solution to this problem
+        case .value(.binding):
+            return evaluate(for: expression)
+
         case let .value(.literal(literal)):
             return transform(literal.evaluate())
         case let .operation(operation):
