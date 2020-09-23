6c9bac4bdf56d64c0a9c8473a2653b7fd0900df8
Luiz Gustavo Gomes Borges <luiz.borges@zup.com.br>
2020-09-23 10:40:40 -0300

    Extracted Flex sub classes to their own files.
    Signed-off-by: Luiz Gustavo Gomes Borges <luiz.borges@zup.com.br>

---
+++ schema/kotlin-core/src/main/kotlin/br/com/zup/beagle/widget/core/AlignSelf.kt
@@ -0,0 +1,63 @@
+/*
+ * Copyright 2020 ZUP IT SERVICOS EM TECNOLOGIA E INOVACAO SA
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package br.com.zup.beagle.widget.core
+
+/**
+ *Describes how to align the children on the container's cross axis.
+ * Align self replaces any parent-defined options with align items.
+ *For example, you can use this property to center a child horizontally
+ *inside a container with flexDirection set to column or vertically inside a container with flexDirection set to row.
+ *
+ * @property FLEX_START
+ * @property CENTER
+ * @property FLEX_END
+ * @property BASELINE
+ * @property AUTO
+ * @property STRETCH
+ */
+enum class AlignSelf {
+    /**
+     * Align wrapped lines to the start of the container's cross axis.
+     */
+    FLEX_START,
+
+    /**
+     * Align wrapped lines in the center of the container's cross axis.
+     */
+    CENTER,
+
+    /**
+     * Align wrapped lines to the end of the container's cross axis.
+     */
+    FLEX_END,
+
+    /**
+     * Align children of a container along a common baseline.
+     * Individual children can be set to be the reference baseline for their parents.
+     */
+    BASELINE,
+
+    /**
+     * Computes to the parent's
+     */
+    AUTO,
+
+    /**
+     * Stretch wrapped lines to match the height of the container's cross axis.
+     */
+    STRETCH
+}
