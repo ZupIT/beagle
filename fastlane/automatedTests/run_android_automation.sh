989a4a238d73d41eb45e4596ffdf9552c8ec83ab
Lucas Sousa Silva <lucas.silva@zup.com.br>
2020-09-26 13:29:46 -0300

    Adding some comments to android automated tests script

--- fastlane/automatedTests/run_android_automation.sh
+++ fastlane/automatedTests/run_android_automation.sh
@@ -13,17 +13,22 @@ function cleanup() {
 trap exit SIGHUP SIGINT
 trap cleanup EXIT
 
+echo "Step /tools/bin/sdkmanager"
 "$ANDROID_SDK_ROOT"/tools/bin/sdkmanager "$AVD_IMAGE"
 
 if "$ANDROID_SDK_ROOT"/emulator/emulator -list-avds | grep -q "$AVD_NAME"; then
     echo "Using avd from cache"
 else
+    echo "Step create AVD"
     #blank line necessary as input to AVD
     echo | "$ANDROID_SDK_ROOT"/tools/bin/avdmanager create avd -n $AVD_NAME -k "$AVD_IMAGE"
 fi
 
+echo "Step start AVD"
 nohup "$ANDROID_SDK_ROOT"/emulator/emulator -avd $AVD_NAME -no-audio -no-boot-anim -no-window -skin 450x800 2>&1 &
 
+
+echo "Step Wait for device"
 "$ANDROID_SDK_ROOT"/platform-tools/adb wait-for-device shell <<ENDSCRIPT
 echo -n "Waiting for device to boot "
 echo "" > /data/local/tmp/zero
@@ -41,6 +46,5 @@ ENDSCRIPT
 echo "Waiting 30 secs for us to be really booted"
 sleep 30
 echo "Starting android automated tests"
-echo "ANDROID_DIR = $ANDROID_DIR"
 $ANDROID_DIR/gradlew -p $ANDROID_DIR/automated-tests connectedAndroidTest --debug
 echo "Finished android automated tests"
