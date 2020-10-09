91aaf423fffdde714abdd32c078b5ebcef18e36a
Tiago Peres França <tiagoperesfr@gmail.com>
2020-10-09 12:54:47 -0300

    chore: adds support for web to the image test (#1022)

--- backend/automated-tests/src/main/kotlin/br/com/zup/beagle/automatedtests/constants/ImageConstants.kt
+++ backend/automated-tests/src/main/kotlin/br/com/zup/beagle/automatedtests/constants/ImageConstants.kt
@@ -17,5 +17,7 @@
 
 const val LOGO_BEAGLE = "imageBeagle"
 
+const val LOGO_BEAGLE_URL = "/public/logo.png"
+
 const val BEACH_NETWORK_IMAGE = "https://www.guiaviagensbrasil.com/imagens/Imagem%20do%20mar%20calma%20e%20belo%20da" +
     "%20Praia%20da%20Engenhoca-Itacar%C3%A9-Bahia-BA.jpg"
