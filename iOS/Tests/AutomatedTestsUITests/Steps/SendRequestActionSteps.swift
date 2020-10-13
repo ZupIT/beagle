81920a9972aefd2aa029a5cd5d928e50ab29dd3a
Lucas Fonseca <lucas.fonseca@zup.com.br>
2020-10-13 17:55:12 -0300

    Send request action steps

---
+++ iOS/Tests/AutomatedTestsUITests/Steps/SendRequestActionSteps.swift
@@ -0,0 +1,9 @@
+//
+//  SendRequestActionSteps.swift
+//  AutomatedTestsUITests
+//
+//  Created by Lucas Cesar on 13/10/20.
+//  Copyright © 2020 ZUP IT SERVICOS EM TECNOLOGIA E INOVACAO SA. All rights reserved.
+//
+
+import Foundation
