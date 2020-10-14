19b1761825ee8f4a5ba59c34765bcec780ca4703
jenifersantoszup <jenifer.santos@zup.com.br>
2020-10-14 18:14:34 -0300

    SendRequest test scenarios
    Signed-off-by: jenifersantoszup <jenifer.santos@zup.com.br>

--- android/automated-tests/src/androidTest/kotlin/br/com/zup/beagle/automatedTests/cucumber/steps/Instrumentation.kt
+++ android/automated-tests/src/androidTest/kotlin/br/com/zup/beagle/automatedTests/cucumber/steps/Instrumentation.kt
@@ -24,7 +24,7 @@ import cucumber.api.android.CucumberInstrumentationCore
 
 @CucumberOptions(
     features = ["features"],
-    tags = ["@regression"],
+    tags = ["@sendrequest"],
     glue = ["br.com.zup.beagle.automatedTests.cucumber.steps"])
 
 class Instrumentation : MonitoringInstrumentation() {
