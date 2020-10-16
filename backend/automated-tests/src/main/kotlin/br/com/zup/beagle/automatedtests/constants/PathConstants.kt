1c05f7e416466b777fdca12e261ddc078ba19c92
Renata Andrade <renata.andrade@zup.com.br>
2020-10-16 20:09:13 -0300

    Adjusts files after merge.

--- backend/automated-tests/src/main/kotlin/br/com/zup/beagle/automatedtests/constants/PathConstants.kt
+++ backend/automated-tests/src/main/kotlin/br/com/zup/beagle/automatedtests/constants/PathConstants.kt
@@ -50,7 +50,6 @@ const val PATH_SCREEN_DEEP_LINK_ENDPOINT = "screen-deep-link"
 
 const val NAVIGATE_ACTIONS_ENDPOINT = "/navigate-actions"
 const val NAVIGATE_RESET_ENDPOINT = "/navigate-reset"
-const val NAVIGATE_RESET_ENDPOINT = "/navigate-reset"
 const val GLOBAL_TEXT_ENDPOINT = "/global-text"
 const val GLOBAL_TEXT_EXPRESSION_ENDPOINT = "@{relativePath}"
 const val SEND_REQUEST_ACTION_ENDPOINT = "/send-request"
