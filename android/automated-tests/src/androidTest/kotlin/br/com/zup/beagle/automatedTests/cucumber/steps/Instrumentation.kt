d432874a8ea807e677de0d8a780f4dceb87d3bca
carlossteinzup <carlos.stein@zup.com.br>
2020-10-19 17:47:55 -0300

    Container tests android ok

--- android/automated-tests/src/androidTest/kotlin/br/com/zup/beagle/automatedTests/cucumber/steps/Instrumentation.kt
+++ android/automated-tests/src/androidTest/kotlin/br/com/zup/beagle/automatedTests/cucumber/steps/Instrumentation.kt
@@ -23,7 +23,7 @@ import cucumber.api.android.CucumberInstrumentationCore
 
 @CucumberOptions(
     features = ["features"],
-    tags = ["@regression"],
+    tags = ["@container"],
     glue = ["br.com.zup.beagle.automatedTests.cucumber.steps"])
 
 class Instrumentation : MonitoringInstrumentation() {
--- android/automated-tests/src/main/kotlin/br/com/zup/beagle/automatedTests/activity/MainActivity.kt
+++ android/automated-tests/src/main/kotlin/br/com/zup/beagle/automatedTests/activity/MainActivity.kt
@@ -40,5 +40,5 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    fun bffUrl() = intent.extras?.getString(BFF_URL_KEY) ?: "http://10.0.2.2:8080/navigate-actions"
+    fun bffUrl() = intent.extras?.getString(BFF_URL_KEY) ?: "http://10.0.2.2:8080/container-test"
 }
