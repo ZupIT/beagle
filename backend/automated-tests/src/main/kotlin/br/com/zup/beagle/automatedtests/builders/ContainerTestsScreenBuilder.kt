01ce1d8eb6888a5d5f1e166e76ae9782bef0ed5e
carlossteinzup <carlos.stein@zup.com.br>
2020-10-19 11:36:11 -0300

    ContainerTestsScreen created

---
+++ backend/automated-tests/src/main/kotlin/br/com/zup/beagle/automatedtests/builders/ContainerTestsScreenBuilder.kt
@@ -0,0 +1,68 @@
+/*
+ * Copyright 2020 ZUP IT SERVICOS EM TECNOLOGIA E INOVACAO SA
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package br.com.zup.beagle.automatedtests.builders
+
+import GLOBAL_TEXT_ENDPOINT
+import GLOBAL_TEXT_EXPRESSION_ENDPOINT
+import br.com.zup.beagle.ext.setId
+import br.com.zup.beagle.widget.action.Alert
+import br.com.zup.beagle.widget.action.Navigate
+import br.com.zup.beagle.widget.action.Route
+import br.com.zup.beagle.widget.action.SetContext
+import br.com.zup.beagle.widget.context.ContextData
+import br.com.zup.beagle.widget.layout.Screen
+import br.com.zup.beagle.widget.layout.Container
+import br.com.zup.beagle.widget.ui.Button
+import br.com.zup.beagle.widget.ui.Text
+
+object ContainerTestsScreenBuilder {
+
+    fun build() = Screen(
+        child = Container(
+            context = ContextData(
+                id = "containerContext",
+                value = "containerContextValue"
+            ),
+            onInit = listOf(
+                Alert(
+                    title = "1stActionExecuted",
+                    message = "Alert called"
+                ),
+                SetContext(
+                    contextId = "global",
+                    value = "2ndActionExecuted"
+                )
+            ),
+            children =
+            listOf(
+                Text(text = "Container Screen"),
+                Text(text = "item1"),
+                Text(text = "item2"),
+                Text(text = "item3"),
+                containerCallingContexts()
+            )
+        )
+    )
+
+    private fun containerCallingContexts(): Container = Container(
+        children = listOf(
+            Text(text = "@{containerContext}"+"1"),
+            Text(text = "@{containerContext}"+"2"),
+            Text(text = "@{global}")
+        )
+    )
+}
