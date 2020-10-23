765db016821fd0aad058fdc24264ca53e2715f4e
jenifersantoszup <jenifer.santos@zup.com.br>
2020-10-23 15:29:32 -0300

    First test textInput
    Signed-off-by: jenifersantoszup <jenifer.santos@zup.com.br>

---
+++ android/automated-tests/src/androidTest/kotlin/br/com/zup/beagle/automatedTests/cucumber/steps/TextInputScreen.kt
@@ -0,0 +1,43 @@
+package br.com.zup.beagle.automatedTests.cucumber.steps
+
+import androidx.test.rule.ActivityTestRule
+import br.com.zup.beagle.automatedTests.activity.MainActivity
+import br.com.zup.beagle.automatedTests.cucumber.elements.*
+import br.com.zup.beagle.automatedTests.cucumber.robots.ScreenRobot
+import br.com.zup.beagle.automatedTests.utils.ActivityFinisher
+import br.com.zup.beagle.automatedTests.utils.TestUtils
+import cucumber.api.java.After
+import cucumber.api.java.Before
+import cucumber.api.java.en.Given
+import cucumber.api.java.en.Then
+import cucumber.api.java.en.When
+import org.junit.Rule
+
+const val TEXT_INPUT_SCREEN_BFF_URL = "http://10.0.2.2:8080/textinput"
+
+class TextInputScreen {
+    @Rule
+    var activityTestRule = ActivityTestRule(MainActivity::class.java)
+
+    @Before("@textInput")
+    fun setup() {
+        TestUtils.startActivity(activityTestRule, TEXT_INPUT_SCREEN_BFF_URL)
+    }
+
+    @Given("^the Beagle application did launch with the textInput on screen$")
+    fun checkBaseScreen() {
+        ScreenRobot()
+            .checkViewContainsText("Beagle Text Input", true)
+    }
+
+    @Then("^I must check if the text (.*) appears on the screen$")
+    fun checkGlobalTextScreen(string2:String) {
+        ScreenRobot()
+            .checkViewContainsText(string2, true)
+    }
+
+    @After("@textInput")
+    fun tearDown() {
+        ActivityFinisher.finishOpenActivities()
+    }
+}
