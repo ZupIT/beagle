5f0566066c02c3a484fdc1c674da513d1f18900a
carlossteinzup <carlos.stein@zup.com.br>
2020-10-30 11:54:14 -0300

    Refactor on textScreenBuilder

--- backend/automated-tests/src/main/kotlin/br/com/zup/beagle/automatedtests/builders/TextScreenBuilder.kt
+++ backend/automated-tests/src/main/kotlin/br/com/zup/beagle/automatedtests/builders/TextScreenBuilder.kt
@@ -38,6 +38,7 @@ object TextScreenBuilder {
 
         child = Container(
             children = listOf(
+                Text("TextScreen"),
                 textRendering(),
                 textColoring(),
                 textAligning()
