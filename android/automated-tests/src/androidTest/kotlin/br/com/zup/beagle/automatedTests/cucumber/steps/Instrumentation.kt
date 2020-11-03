834e224e925b4c5e93b4b422f9b42c53c16f3c46
carlossteinzup <carlos.stein@zup.com.br>
2020-11-03 14:02:52 -0300

    Alert tests ok

--- android/automated-tests/src/androidTest/kotlin/br/com/zup/beagle/automatedTests/cucumber/steps/Instrumentation.kt
+++ android/automated-tests/src/androidTest/kotlin/br/com/zup/beagle/automatedTests/cucumber/steps/Instrumentation.kt
@@ -24,7 +24,7 @@ import cucumber.api.android.CucumberInstrumentationCore
 
 @CucumberOptions(
     features = ["features"],
-    tags = ["@regression"],
+    tags = ["@alert"],
     glue = ["br.com.zup.beagle.automatedTests.cucumber.steps"])
 
 class Instrumentation : MonitoringInstrumentation() {
