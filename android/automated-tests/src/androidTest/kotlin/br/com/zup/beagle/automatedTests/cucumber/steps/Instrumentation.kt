e637a81cdf60e7dbc3c6b83eafd476add9d8eb2a
carlossteinzup <carlos.stein@zup.com.br>
2020-11-04 14:59:29 -0300

    regression tests ok

--- android/automated-tests/src/androidTest/kotlin/br/com/zup/beagle/automatedTests/cucumber/steps/Instrumentation.kt
+++ android/automated-tests/src/androidTest/kotlin/br/com/zup/beagle/automatedTests/cucumber/steps/Instrumentation.kt
@@ -23,7 +23,7 @@ import cucumber.api.android.CucumberInstrumentationCore
 
 @CucumberOptions(
     features = ["features"],
-    tags = ["@text"],
+    tags = ["@regression"],
     glue = ["br.com.zup.beagle.automatedTests.cucumber.steps"])
 
 class Instrumentation : MonitoringInstrumentation() {
