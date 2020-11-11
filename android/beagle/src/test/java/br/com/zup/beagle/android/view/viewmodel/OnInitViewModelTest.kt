e5510afb37b920ca76841d5ef6fcd8ccdf3738df
Matheus Ribeiro <matheus.ribeiro@zup.com.br>
2020-11-10 23:26:08 -0300

    feat: added OnInitViewModel to handle onInit status
    Signed-off-by: Matheus Ribeiro <matheus.ribeiro@zup.com.br>

---
+++ android/beagle/src/test/java/br/com/zup/beagle/android/view/viewmodel/OnInitViewModelTest.kt
@@ -0,0 +1,52 @@
+/*
+ * Copyright 2020 ZUP IT SERVICOS EM TECNOLOGIA E INOVACAO SA
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package br.com.zup.beagle.android.view.viewmodel
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertFalse
+import org.junit.jupiter.api.Test
+
+class OnInitViewModelTest {
+
+    private val onInitViewModel = OnInitViewModel()
+
+    @Test
+    fun `GIVEN onInitiableComponentId WHEN setOnInitActionStatus THEN should set to onInitStatusByComponent id the given value`() {
+        // Given
+        val onInitiableComponentId = 10
+        val onInitCalled = true
+
+        // When
+        onInitViewModel.setOnInitActionStatus(onInitiableComponentId, onInitCalled)
+        val result = onInitViewModel.getOnInitActionStatus(onInitiableComponentId)
+
+        // Then
+        assertEquals(onInitCalled, result)
+    }
+
+    @Test
+    fun `GIVEN onInitiableComponentId not found WHEN getOnInitActionStatus THEN should return false`() {
+        // Given
+        val onInitiableComponentId = 10
+
+        // When
+        val result = onInitViewModel.getOnInitActionStatus(onInitiableComponentId)
+
+        // Then
+        assertFalse(result)
+    }
+}
