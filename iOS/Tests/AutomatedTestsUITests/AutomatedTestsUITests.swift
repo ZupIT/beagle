b88007400c462f7e4cffd577de741f630e1e4745
Gabriela Coelho <gabriela.coelho@zup.com.br>
2020-11-13 11:33:43 -0300

    adding ios tests

--- iOS/Tests/AutomatedTestsUITests/AutomatedTestsUITests.swift
+++ iOS/Tests/AutomatedTestsUITests/AutomatedTestsUITests.swift
@@ -35,6 +35,7 @@ class CucumberishInitializer: NSObject {
                     TextInputSteps(),
                     TextSteps(),
                     AlertSteps(),
+                    ConfirmActionSteps(),
                     ConditionalActionSteps()
                 ]
             for stepsDefinition in stepsDefinitions {
