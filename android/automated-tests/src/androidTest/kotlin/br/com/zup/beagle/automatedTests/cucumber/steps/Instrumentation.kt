59e7a82d4ca570d57ea8f685d31850ba1bcd369f
carlossteinzup <carlos.stein@zup.com.br>
2020-11-20 18:20:08 -0300

    refactor tabbar screen to implement web tests

--- android/automated-tests/src/androidTest/kotlin/br/com/zup/beagle/automatedTests/cucumber/steps/Instrumentation.kt
+++ android/automated-tests/src/androidTest/kotlin/br/com/zup/beagle/automatedTests/cucumber/steps/Instrumentation.kt
@@ -23,7 +23,7 @@ import cucumber.api.android.CucumberInstrumentationCore
 
 @CucumberOptions(
     features = ["features"],
-    tags = ["@regression"],
+    tags = ["@lazyComponent"],
     glue = ["br.com.zup.beagle.automatedTests.cucumber.steps"])
 
 class Instrumentation : MonitoringInstrumentation() {
