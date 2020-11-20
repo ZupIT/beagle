e4c72eff11c2117138f4d4299426eca483b4277c
jenifersantoszup <jenifer.santos@zup.com.br>
2020-11-20 14:37:14 -0300

    Returning tags to default
    Signed-off-by: jenifersantoszup <jenifer.santos@zup.com.br>

--- android/automated-tests/src/androidTest/kotlin/br/com/zup/beagle/automatedTests/cucumber/steps/Instrumentation.kt
+++ android/automated-tests/src/androidTest/kotlin/br/com/zup/beagle/automatedTests/cucumber/steps/Instrumentation.kt
@@ -23,7 +23,7 @@ import cucumber.api.android.CucumberInstrumentationCore
 
 @CucumberOptions(
     features = ["features"],
-    tags = ["@scrollview"],
+    tags = ["@regression"],
     glue = ["br.com.zup.beagle.automatedTests.cucumber.steps"])
 
 class Instrumentation : MonitoringInstrumentation() {
