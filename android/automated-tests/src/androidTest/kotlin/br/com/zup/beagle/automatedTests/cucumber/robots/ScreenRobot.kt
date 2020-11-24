e94056ef34776c84c39b2308db9c32e3480a0736
carlossteinzup <carlos.stein@zup.com.br>
2020-11-24 14:13:06 -0300

    Rregression tests ok

--- android/automated-tests/src/androidTest/kotlin/br/com/zup/beagle/automatedTests/cucumber/robots/ScreenRobot.kt
+++ android/automated-tests/src/androidTest/kotlin/br/com/zup/beagle/automatedTests/cucumber/robots/ScreenRobot.kt
@@ -26,7 +26,13 @@ import androidx.test.espresso.action.ViewActions.pressBack
 import androidx.test.espresso.action.ViewActions.scrollTo
 import androidx.test.espresso.assertion.ViewAssertions.doesNotExist
 import androidx.test.espresso.assertion.ViewAssertions.matches
-import androidx.test.espresso.matcher.ViewMatchers.*
+import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
+import androidx.test.espresso.matcher.ViewMatchers.isEnabled
+import androidx.test.espresso.matcher.ViewMatchers.withClassName
+import androidx.test.espresso.matcher.ViewMatchers.withHint
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.matcher.ViewMatchers.withInputType
+import androidx.test.espresso.matcher.ViewMatchers.withText
 import br.com.zup.beagle.automatedTests.R
 import br.com.zup.beagle.automatedTests.utils.WaitHelper
 import br.com.zup.beagle.automatedTests.utils.matcher.MatcherExtension
