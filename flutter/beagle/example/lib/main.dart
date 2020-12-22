2f8e9b4d52045122252000845b344734d11220c1
uziasferreirazup <uzias.ferreira@zup.com.br>
2020-12-22 17:15:11 -0400

    remove beagle core js and put in beagle

---
+++ flutter/beagle/example/lib/main.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+// ignore: use_key_in_widget_constructors
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Plugin example app'),
+        ),
+        body: const Center(
+          child: Text('Running'),
+        ),
+      ),
+    );
+  }
+}
--- flutter/beagle_core_js/example/android/app/src/main/kotlin/br/com/zup/beagle/beagle_core_js1_example/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package br.com.zup.beagle.beagle_core_js1_example
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
