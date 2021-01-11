4f969bb9a254f6b4b65d1e637e175341fd8b5c52
jenifersantoszup <jenifer.santos@zup.com.br>
2021-01-11 14:06:03 -0300

    Unit test added to verify that the component was returned correctly
    Signed-off-by: jenifersantoszup <jenifer.santos@zup.com.br>

---
+++ android/beagle/src/test/java/br/com/zup/beagle/android/engine/renderer/ViewRendererFactoryTest.kt
@@ -0,0 +1,56 @@
+/*
+ * Copyright 2020 ZUP IT SERVICOS EM TECNOLOGIA E INOVACAO SA
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package br.com.zup.beagle.android.engine.renderer
+
+import br.com.zup.beagle.android.BaseTest
+import br.com.zup.beagle.android.components.Text
+import br.com.zup.beagle.android.widget.ViewConvertable
+import org.junit.Assert
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Nested
+import org.junit.jupiter.api.Test
+
+private const val TEXT = "test"
+private lateinit var component: Text
+private val viewRendererFactory = ViewRendererFactory()
+
+@DisplayName("Given a ViewRendererFactory")
+class ViewRendererFactoryTest : BaseTest() {
+
+    @BeforeEach
+    override fun setUp() {
+        super.setUp()
+        component = Text(TEXT)
+    }
+
+    @DisplayName("When to pass the component")
+    @Nested
+    inner class ComponentConversion {
+
+        @DisplayName("Then the component should be return correctly")
+        @Test
+        fun checkComponentCreation() {
+            // When
+            val result = viewRendererFactory.make(component)
+
+            //Then
+            Assert.assertEquals(result.component, component as ViewConvertable)
+        }
+    }
+
+}
