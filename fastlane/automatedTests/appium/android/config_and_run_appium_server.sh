66e10953c2e640ef887c641636b869b46a9c3317
igorgiumellizup <igor.giumelli@zup.com.br>
2021-01-14 17:34:42 -0300

    permission

--- fastlane/automatedTests/appium/android/config_and_run_appium_server.sh
+++ fastlane/automatedTests/appium/android/config_and_run_appium_server.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -ex
 
-sudo chown -R 777 /usr/local/
-npm install -g appium --unsafe-perm=true --allow-root
+sudo chown -R runner:runner /usr/local/
+sudo npm install -g appium --unsafe-perm=true --allow-root
 appium -v
 appium &>/dev/null &
