ea5666b4aac213a42279e43cbb80d70f312ac0db
igorgiumellizup <igor.giumelli@zup.com.br>
2021-01-28 18:37:00 -0300

    fix script

--- fastlane/automatedTests/appium/ios/run_appium_ios_tests.sh
+++ fastlane/automatedTests/appium/ios/run_appium_ios_tests.sh
@@ -20,8 +20,7 @@ set -e
 
 APPIUM_PROJECT_DIR=tests/appium/project
 
-APP_FILE=$(find ~/Library/Developer/Xcode/DerivedData -name AppiumApp.app \ 
- | grep Build/Products/Debug-iphonesimulator/AppiumApp.app)
+APP_FILE=$(find ~/Library/Developer/Xcode/DerivedData -name AppiumApp.app | grep Build/Products/Debug-iphonesimulator/AppiumApp.app)
 
 if [ -z "$APP_FILE" ]; then
    echo "app file not found!"
