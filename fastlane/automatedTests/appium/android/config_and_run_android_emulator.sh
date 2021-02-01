5b585e638742821c1d2892ec4282576fb5a74e40
igorgiumellizup <igor.giumelli@zup.com.br>
2021-02-01 10:03:48 -0300

    old script update

--- fastlane/automatedTests/appium/android/config_and_run_android_emulator.sh
+++ fastlane/automatedTests/appium/android/config_and_run_android_emulator.sh
@@ -21,7 +21,7 @@ set -e
 AVD_NAME='Pixel_3a_API_30_x86'
 AVD_IMAGE='system-images;android-30;google_apis;x86'
 APP_ANDROID_DIR=tests/appium/app-android
-APP_ANDROID_APK_FILE=$APP_ANDROID_DIR/app/build/outputs/apk/debug/app-debug.apk
+APP_ANDROID_APK_FILE=$GITHUB_WORKSPACE/$APP_ANDROID_DIR/app/build/outputs/apk/debug/app-debug.apk
 AVD_CONFIG_FILE=~/.android/avd/$AVD_NAME.avd/config.ini
 
 function checkFileExists(){
@@ -42,18 +42,18 @@ $APP_ANDROID_DIR/gradlew -p $APP_ANDROID_DIR assembleDebug
 checkFileExists $APP_ANDROID_APK_FILE
 
 echo "##### Installing / updating image $AVD_IMAGE ..."
-"$ANDROID_SDK_ROOT"/tools/bin/sdkmanager "$AVD_IMAGE"
-"$ANDROID_SDK_ROOT"/tools/bin/sdkmanager --update
+"$ANDROID_HOME"/tools/bin/sdkmanager "$AVD_IMAGE"
+"$ANDROID_HOME"/tools/bin/sdkmanager --update
 
 #echo "##### Emulator version:"
-$ANDROID_SDK_ROOT/emulator/emulator -version
+$ANDROID_HOME/emulator/emulator -version
 
-if "$ANDROID_SDK_ROOT"/emulator/emulator -list-avds | grep -q "$AVD_NAME"; then
+if "$ANDROID_HOME"/emulator/emulator -list-avds | grep -q "$AVD_NAME"; then
     echo "##### Using avd from cache"
 else
     echo "##### AVD not found in cache, creating AVD ..."
     #blank line necessary as input to AVD
-    echo n | "$ANDROID_SDK_ROOT"/tools/bin/avdmanager create avd -f -n $AVD_NAME -k "$AVD_IMAGE"
+    echo n | "$ANDROID_HOME"/tools/bin/avdmanager create avd -f -n $AVD_NAME -k "$AVD_IMAGE"
 fi
 
 echo "##### Checking if AVD was created correctly ..."
@@ -69,14 +69,14 @@ hw.gpu.enabled = yes
 hw.gpu.mode = auto" >> $AVD_CONFIG_FILE
 
 #echo "##### Checking if a hypervisor is installed"
-#"$ANDROID_SDK_ROOT"/emulator/emulator -accel-check
+#"$ANDROID_HOME"/emulator/emulator -accel-check
 
 echo "##### Starting emulator with AVD ..."
-nohup "$ANDROID_SDK_ROOT"/emulator/emulator -avd $AVD_NAME -no-window -no-snapshot -noaudio -no-boot-anim 2>&1 &
+nohup "$ANDROID_HOME"/emulator/emulator -avd $AVD_NAME -no-window -no-snapshot -noaudio -no-boot-anim 2>&1 &
 
 
 echo "##### Waiting for device to boot"
-"$ANDROID_SDK_ROOT"/platform-tools/adb wait-for-device shell <<ENDSCRIPT
+"$ANDROID_HOME"/platform-tools/adb wait-for-device shell <<ENDSCRIPT
 echo "" > /data/local/tmp/zero
 getprop dev.bootcomplete > /data/local/tmp/bootcomplete
 while cmp /data/local/tmp/zero /data/local/tmp/bootcomplete; do
@@ -89,17 +89,17 @@ echo "Booted."
 exit
 ENDSCRIPT
 
-echo "#### Waiting 30 secs for us to be really booted ..."
-sleep 30
+echo "#### Giving additional 15s for the device to finish booting and setting up  ..."
+sleep 15
 
 echo "##### Installing the .apk file in the emulator ..."
-$ANDROID_SDK_ROOT/platform-tools/adb install -r -t $APP_ANDROID_APK_FILE
+$ANDROID_HOME/platform-tools/adb install -r -t $APP_ANDROID_APK_FILE
 
 echo "##### Waiting 30 secs for the app to be installed ..."
 sleep 30
 
 echo "##### Checking if the .apk was installed in the emulator ... "
-OUTPUT=`$ANDROID_SDK_ROOT/platform-tools/adb shell pm list packages`
+OUTPUT=`$ANDROID_HOME/platform-tools/adb shell pm list packages`
 if echo "$OUTPUT" | grep -q "package:br.com.zup.beagle.appiumapp"; then
     echo "OK!"
 else 
@@ -107,6 +107,6 @@ else
 	exit 1
 fi
 
-echo "##### AVD settings: "
+echo "##### AVD settings after he process: "
 cat $AVD_CONFIG_FILE
 
