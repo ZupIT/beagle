3864662959699038d0c323e32fff4057bcaa9dbd
igorgiumellizup <igor.giumelli@zup.com.br>
2021-02-04 16:26:48 -0300

    script update

--- fastlane/automatedTests/appium/setup_apk_in_browserstack_and_run_android_tests.sh
+++ fastlane/automatedTests/appium/setup_apk_in_browserstack_and_run_android_tests.sh
@@ -17,6 +17,10 @@
 #
 
 set -e
+
+APP_FILE=$GITHUB_WORKSPACE/tests/appium/app-android/app/build/outputs/apk/debug/app-debug.apk
+
+echo "Uploading .apk file in BrowserStack..."
 APP_UPLOAD_RESPONSE=$(curl -u "$BROWSERSTACK_USER:$BROWSERSTACK_KEY" \
 -X POST https://api-cloud.browserstack.com/app-automate/upload \
 -F "file=@$APP_FILE")
@@ -25,11 +29,23 @@ APP_ID=$(echo $APP_UPLOAD_RESPONSE | jq -r ".app_url")
 
 if [ $APP_ID != null ]; then
   echo "Apk uploaded to BrowserStack!"
-  ${BROWSERSTACK_APP_ID:=$APP_ID}
 else
   UPLOAD_ERROR_MESSAGE=$(echo $APP_UPLOAD_RESPONSE | jq -r ".error")
   echo "App upload failed, reason : ",$UPLOAD_ERROR_MESSAGE
   exit 1;
 fi
 
-
+echo "Running Appium tests..."
+if ./tests/appium/project/gradlew --build-cache -p tests/appium/project cucumber \
+-Dplatform="android" \
+-Dplatform_version="11.0" \
+-Ddevice_name="Google Pixel 4" \
+-Dbrowserstack.user="$BROWSERSTACK_USER" \
+-Dbrowserstack.key="$BROWSERSTACK_KEY" \
+-Dapp_file="$APP_ID" \
+-Dbff_base_url="$BFF_URL"; then
+  echo "Gradle task succeeded" >&2
+else
+  echo "Gradle task failed" >&2
+  exit 1
+fi
