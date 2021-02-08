4d7e4d210ec06eaf8ba4bfeed61b7768d696a3e7
Vinicius Guardieiro Sousa <vinicius.guardieiro@zup.com.br>
2021-02-08 15:14:27 -0300

    remove line

--- android/beagle/src/test/java/br/com/zup/beagle/android/utils/StringExtensionsKtTest.kt
+++ android/beagle/src/test/java/br/com/zup/beagle/android/utils/StringExtensionsKtTest.kt
@@ -37,7 +37,7 @@ import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.Assertions.assertEquals
 
-class StringExtensionsKtTest {
+class StringExtensionsKtTest{
 
     private val colorSlot = slot<String>()
 
