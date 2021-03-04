a5d3b71d0ee0e07b9734442a01e32bab06601bd5
Vitor Basso <vitor.basso@zup.com.br>
2021-03-04 16:32:48 -0300

    Update BeagleMicronautCacheHandlerTest.kt

--- backend/starters/micronaut/src/test/kotlin/br/com/zup/beagle/micronaut/filter/BeagleMicronautCacheHandlerTest.kt
+++ backend/starters/micronaut/src/test/kotlin/br/com/zup/beagle/micronaut/filter/BeagleMicronautCacheHandlerTest.kt
@@ -1,55 +1,67 @@
-///*
-// * Copyright 2020 ZUP IT SERVICOS EM TECNOLOGIA E INOVACAO SA
-// *
-// * Licensed under the Apache License, Version 2.0 (the "License");
-// * you may not use this file except in compliance with the License.
-// * You may obtain a copy of the License at
-// *
-// *     http://www.apache.org/licenses/LICENSE-2.0
-// *
-// * Unless required by applicable law or agreed to in writing, software
-// * distributed under the License is distributed on an "AS IS" BASIS,
-// * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// * See the License for the specific language governing permissions and
-// * limitations under the License.
-// */
-//
-//package br.com.zup.beagle.micronaut.filter
-//
-//import br.com.zup.beagle.micronaut.STRING
-//import io.micronaut.http.HttpStatus
-//import io.micronaut.http.MutableHttpResponse
-//import io.micronaut.http.filter.ServerFilterChain
-//import io.mockk.every
-//import io.mockk.mockk
-//import io.mockk.verifyAll
-//import io.reactivex.Flowable
-//import org.junit.jupiter.api.Test
-//import java.util.Optional
-//import kotlin.test.assertEquals
-//import kotlin.test.assertSame
-//
-//internal class BeagleMicronautCacheHandlerTest {
-//    @Test
-//    fun `Test createResponseFromController`() {
-//        val chain = mockk<ServerFilterChain>()
-//        val response = mockk<MutableHttpResponse<*>>()
-//        val publisher = Flowable.just(response)
-//
-//        every { chain.proceed(any()) } returns publisher
-//        val result = BeagleMicronautCacheHandler(mockk(), chain).createResponseFromController()
-//
-//        assertSame(response, result)
-//    }
-//
-//    @Test
-//    fun `Test createResponse`() {
-//        val result = BeagleMicronautCacheHandler(mockk(), mockk()).createResponse(200)
-//
-//        assertEquals(Unit, result.body.get())
-//        assertEquals(HttpStatus.OK, result.status)
-//    }
-//
+/*
+ * Copyright 2020 ZUP IT SERVICOS EM TECNOLOGIA E INOVACAO SA
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package br.com.zup.beagle.micronaut.filter
+
+import br.com.zup.beagle.micronaut.configuration.BeagleMicronautCacheProperties
+import io.micronaut.http.HttpResponseFactory
+import io.micronaut.http.HttpStatus
+import io.micronaut.http.MutableHttpResponse
+import io.micronaut.http.filter.ServerFilterChain
+import io.mockk.every
+import io.mockk.mockk
+import io.reactivex.Flowable
+import org.junit.jupiter.api.Test
+import kotlin.test.assertEquals
+
+internal class BeagleMicronautCacheHandlerTest {
+    @Test
+    fun `Test createResponseFromController`() {
+        val chain = mockk<ServerFilterChain>()
+        val response: MutableHttpResponse<*> = HttpResponseFactory.INSTANCE.status(HttpStatus.OK, Unit)
+        val publisher = Flowable.just(response)
+
+        fun testingFunction(input: Any): Any {
+            return (input as MutableHttpResponse<*>).status(HttpStatus.FOUND)
+        }
+
+        every { chain.proceed(any()) } returns publisher
+        val result = BeagleMicronautCacheHandler(
+            request = mockk(),
+            chain = chain,
+            properties = BeagleMicronautCacheProperties()
+        ).createResponseFromController{
+            (it as MutableHttpResponse<*>).status(HttpStatus.FOUND)
+        } as Flowable<MutableHttpResponse<*>>
+
+        val single = result.blockingSingle()
+
+        assertEquals(single.status, HttpStatus.FOUND)
+    }
+
+    @Test
+    fun `Test createResponse`() {
+        val result = BeagleMicronautCacheHandler(mockk(), mockk(), mockk()).createResponse(200)
+
+        val single = result.blockingSingle()
+        assertEquals(HttpStatus.OK, single.status)
+        assertEquals(single.body.get(), Unit)
+
+    }
+
 //    @Test
 //    fun `Test getBody`() {
 //        val response = mockk<MutableHttpResponse<*>>()
@@ -71,4 +83,4 @@
 //
 //        verifyAll { response.header(STRING, STRING) }
 //    }
-//}
+}
