f17d0dcab4e7ac2cb65643846e1b5aac397d55d9
paulomeurerzup <paulo.meurer@zup.com.br>
2021-03-08 14:03:15 -0300

    refactor: remove unused imports
    Signed-off-by: paulomeurerzup <paulo.meurer@zup.com.br>

--- flutter/beagle/lib/beagle_sdk.dart
+++ flutter/beagle/lib/beagle_sdk.dart
@@ -31,7 +31,6 @@ import 'package:beagle/model/beagle_config.dart';
 import 'package:beagle/networking/beagle_network_strategy.dart';
 import 'package:beagle/service_locator.dart';
 import 'package:beagle/setup/beagle_design_system.dart';
-import 'package:flutter/widgets.dart';
 
 class BeagleSdk {
   /// Starts the BeagleService. Only a single instance of this service is allowed.
