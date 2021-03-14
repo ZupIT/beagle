db732c11383119c47c9c06be8a836829c0cbc829
uziasferreirazup <uzias.ferreira@zup.com.br>
2021-03-14 20:49:48 -0300

    remove code android

--- android/sample/src/main/java/br/com/zup/beagle/sample/fragment/ContextOperationsFragment.kt
+++ android/sample/src/main/java/br/com/zup/beagle/sample/fragment/ContextOperationsFragment.kt
@@ -23,8 +23,6 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import br.com.zup.beagle.android.action.SetContext
 import br.com.zup.beagle.android.components.Button
-import br.com.zup.beagle.android.components.Image
-import br.com.zup.beagle.android.components.ImagePath
 import br.com.zup.beagle.core.Style
 import br.com.zup.beagle.android.utils.toView
 import br.com.zup.beagle.widget.core.EdgeValue
@@ -34,82 +32,61 @@ import br.com.zup.beagle.widget.core.UnitValue
 import br.com.zup.beagle.android.components.layout.Container
 import br.com.zup.beagle.android.components.Text
 import br.com.zup.beagle.android.components.TextInput
-import br.com.zup.beagle.android.components.center
-import br.com.zup.beagle.android.components.column
-import br.com.zup.beagle.android.components.layout.Center
-import br.com.zup.beagle.android.components.layout.Column
 import br.com.zup.beagle.android.components.layout.NavigationBar
-import br.com.zup.beagle.android.components.layout.Row
 import br.com.zup.beagle.android.components.layout.Screen
-import br.com.zup.beagle.android.components.styled
-import br.com.zup.beagle.android.context.Bind
 import br.com.zup.beagle.android.context.ContextData
 import br.com.zup.beagle.android.context.expressionOf
-import br.com.zup.beagle.android.context.valueOf
-import br.com.zup.beagle.core.ServerDrivenComponent
-import br.com.zup.beagle.ext.applyFlex
 import br.com.zup.beagle.ext.applyStyle
-import br.com.zup.beagle.widget.core.AlignContent
-import br.com.zup.beagle.widget.core.AlignItems
-import br.com.zup.beagle.widget.core.AlignSelf
-import br.com.zup.beagle.widget.core.FlexDirection
-import br.com.zup.beagle.widget.core.ImageContentMode
-import br.com.zup.beagle.widget.core.JustifyContent
-import br.com.zup.beagle.widget.core.TextAlignment
-
-fun MyButton(text: String) = styled(Button(text = text), {
-    backgroundColor = "#00FFFF"
-    padding = EdgeValue(all = UnitValue(10.0, UnitType.REAL))
-})
-
-fun MyButtonSecond(text: String) = styled(Button(text = text), {
-    backgroundColor = "#0000A0"
-    padding = EdgeValue(all = UnitValue(10.0, UnitType.REAL))
-})
-
-fun MyButtonSecondWithPadding(text: String) = styled(MyButtonSecond(text), {
-    padding = EdgeValue(all = UnitValue(20.0, UnitType.REAL))
-})
-
-fun MyCenter(child: ServerDrivenComponent) = styled(center(child), {
-    padding = EdgeValue(all = UnitValue(20.0, UnitType.REAL))
-})
-
-fun MyImage(
-    path: Bind<ImagePath>,
-    mode: ImageContentMode? = null,
-) = styled(Image(path, mode), {
-    padding = EdgeValue(all = UnitValue(20.0, UnitType.REAL))
-})
-
-fun MySecondImage(
-    path: Bind<ImagePath>,
-    mode: ImageContentMode? = null,
-) = styled(MyImage(path, mode), {
-    padding = EdgeValue(all = UnitValue(20.0, UnitType.REAL))
-})
 
 class ContextOperationsFragment : Fragment() {
 
+    private val style = Style(margin = EdgeValue(all = UnitValue(50.0, UnitType.REAL)), flex = Flex(shrink = 0.0))
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?,
+        savedInstanceState: Bundle?
     ): View? {
         val declarative = screen()
 
         return context?.let { declarative.toView(this) }
     }
 
-    fun screen() = column(
-        listOf(
-            Text("text"),
-            MyImage(path = valueOf(ImagePath.Remote(url = "blabla"))),
-            MyButton("UZIAS"),
-            MyButtonSecond("UZIAS"),
-            MyButtonSecondWithPadding("UZIAS"),
-            center(Text("text")),
-            center(Text("text"))
+    fun screen() = Screen(
+        navigationBar = NavigationBar(
+            title = "Choose a Component",
+            showBackButton = true
         ),
+        child = Container(
+            context = ContextData("counter", 2),
+            children = listOf(
+                Text(expressionOf("Counter: @{sum(2, 1)}")),
+                Text(expressionOf("Counter: @{counter}")),
+                Button(
+                    text = "increment",
+                    onPress = listOf(SetContext("counter", "@{sum(counter, 1)}"))
+                ),
+                Button(
+                    text = "decrement",
+                    onPress = listOf(SetContext("counter", "@{subtract(counter, 1)}"))
+                ),
+                Text(text = "The text bellow will show if the counter + 2 is below 5 or not"),
+                Text(expressionOf(
+                    "@{condition(lt(sum(counter, 2), 5), 'less then 5', 'greater then 5')}")
+                ).applyStyle(Style(backgroundColor = "#00FF00")),
+                Container(
+                    context = ContextData("cpf", ""),
+                    children = listOf(
+                        TextInput(placeholder = "CPF", onChange = listOf(
+                            SetContext(
+                                contextId = "cpf",
+                                value = "@{onChange.value}"
+                            )
+                        )),
+                        Text("@{condition(isValidCpf(cpf), 'cpf is valid', 'cpf is not valid')}")
+                    )
+                )
+            )
+        )
     )
 
     companion object {
