4ba32c31b6a46f12be7b4c8599ff5877b319b217
Uzias Ferreira <63263091+uziasferreirazup@users.noreply.github.com>
2021-03-17 21:52:20 -0300

    Update backend/widgets/src/main/kotlin/br/com/zup/beagle/widget/style/SizedBox.kt
    Co-authored-by: igorgiumellizup <73198010+igorgiumellizup@users.noreply.github.com>

--- backend/widgets/src/main/kotlin/br/com/zup/beagle/widget/style/SizedBox.kt
+++ backend/widgets/src/main/kotlin/br/com/zup/beagle/widget/style/SizedBox.kt
@@ -37,7 +37,7 @@ fun <T : StyleComponent> SizedBox(width: Int,
         self = self)
 
 /**
- *  The SizedBox it is a helper to apply a size in your component
+ *  The SizedBox is a helper to apply a size in your component
  * @param width The value specifies the view's width
  * @param height The value specifies the view's height
  * @param self the component will apply size
