cfa582b228ea4deaaa3e20ca6f96e5ced63275d8
igorgiumellizup <igor.giumelli@zup.com.br>
2021-04-23 10:26:33 -0300

    final adjusts

--- tests/appium/project/src/test/kotlin/br/com/zup/beagle/cucumber/steps/AbstractStep.kt
+++ tests/appium/project/src/test/kotlin/br/com/zup/beagle/cucumber/steps/AbstractStep.kt
@@ -72,7 +72,7 @@ abstract class AbstractStep {
         } else {
             /**
              * Deep link test strategy on iOS (URL Scheme) doesn't boost test speed when tests run on non-gpu Simulators (ex. GitHub Actions)
-             * or real Devices (ex. Browserstack). Because of this, deep link is turned off by default on iOS.
+             * or on real devices (ex. Browserstack). Because of this, deep link is turned off by default on iOS.
              *
              * When testing on a gpu-enabled Simulator (ex. testing locally on a mac computer), the deep link strategy might result on some
              * speed boost. To enable deep link on iOS, use method loadBffScreenFromDeepLink() instead of method loadBffScreenFromMainScreen().
