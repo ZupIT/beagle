9ab09bf1df9c0ec879a64779c63e58aeab82b429
Uzias Ferreira <uziasferreira@gmail.com>
2021-05-17 15:05:44 -0300

    feat: add component grid view in backend

---
+++ backend/widgets/src/main/kotlin/br/com/zup/beagle/widget/ui/GridView.kt
@@ -0,0 +1,37 @@
+/*
+ * Copyright 2020 ZUP IT SERVICOS EM TECNOLOGIA E INOVACAO SA
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package br.com.zup.beagle.widget.ui
+
+import br.com.zup.beagle.core.ServerDrivenComponent
+import br.com.zup.beagle.widget.Widget
+import br.com.zup.beagle.widget.action.Action
+import br.com.zup.beagle.widget.context.Bind
+import br.com.zup.beagle.widget.context.ContextComponent
+import br.com.zup.beagle.widget.context.ContextData
+
+data class GridView constructor(
+    override val context: ContextData? = null,
+    val onInit: List<Action>? = null,
+    val dataSource: Bind<List<Any>>? = null,
+    val template: ServerDrivenComponent? = null,
+    val onScrollEnd: List<Action>? = null,
+    val scrollEndThreshold: Int? = null,
+    val isScrollIndicatorVisible: Boolean = false,
+    val iteratorName: String = "item",
+    val key: String? = null,
+    val numColumns: Int = 0,
+) : Widget(), ContextComponent
