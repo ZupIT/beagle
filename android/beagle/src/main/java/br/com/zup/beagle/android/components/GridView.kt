c7fc67f8dfed0311cf8df210ddb5fc60d682d042
Uzias Ferreira <uziasferreira@gmail.com>
2021-05-17 15:55:13 -0300

    remove construtor

--- android/beagle/src/main/java/br/com/zup/beagle/android/components/GridView.kt
+++ android/beagle/src/main/java/br/com/zup/beagle/android/components/GridView.kt
@@ -41,7 +41,7 @@ import br.com.zup.beagle.widget.core.ListDirection
  * @param numColumns Defines how many columns to show.
  */
 @RegisterWidget("gridView")
-data class GridView constructor(
+data class GridView(
     override val context: ContextData? = null,
     val onInit: List<Action>? = null,
     val dataSource: Bind<List<Any>>? = null,
