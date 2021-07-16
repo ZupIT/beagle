1c63cee603fc11b9fb1c71c9974e6b7b32463c52
igorgiumellizup <igor.giumelli@zup.com.br>
2021-07-16 10:46:02 -0300

    impl

--- fastlane/automatedTests/publish_test_backend.sh
+++ fastlane/automatedTests/publish_test_backend.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-BACKEND_DIR=backend
+BACKEND_DIR=tests/bff-server
 IMAGE_NAME=$(echo "$registry/$repository/$image" | tr [:upper:] [:lower:])
-$BACKEND_DIR/gradlew -p $BACKEND_DIR automated-tests:bootBuildImage --imageName=$IMAGE_NAME
+$BACKEND_DIR/gradlew -p $BACKEND_DIR bff:bootBuildImage --imageName=$IMAGE_NAME
 echo $token | docker login $registry -u $user --password-stdin
 docker push $IMAGE_NAME
 docker logout $registry
