c54bd79bd217fae8e1363be2fa20f27638efdfc3
igorgiumellizup <73198010+igorgiumellizup@users.noreply.github.com>
2021-07-19 09:54:50 -0300

    chore: move backend/automated-tests to tests/bff-server (#1692)
    * impl
    
    * update

--- fastlane/automatedTests/start_test_backend.sh
+++ fastlane/automatedTests/start_test_backend.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-BACKEND_DIR=backend
-$BACKEND_DIR/gradlew -p $BACKEND_DIR automated-tests:bootJar
+BACKEND_DIR=tests/bff-dir
+$BACKEND_DIR/gradlew -p $BACKEND_DIR bff:bootJar
 nohup java -jar $BACKEND_DIR/automated-tests/build/libs/automated-tests.jar 2>&1 &
