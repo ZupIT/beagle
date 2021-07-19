3489f138ff3376e64e5e38a14291f186176161b5
igorgiumellizup <73198010+igorgiumellizup@users.noreply.github.com>
2021-07-19 10:58:04 -0300

    fix: update ci script considering new folder

--- fastlane/automatedTests/start_test_backend.sh
+++ fastlane/automatedTests/start_test_backend.sh
@@ -2,4 +2,4 @@
 
 BACKEND_DIR=tests/bff-dir
 $BACKEND_DIR/gradlew -p $BACKEND_DIR bff:bootJar
-nohup java -jar $BACKEND_DIR/automated-tests/build/libs/automated-tests.jar 2>&1 &
+nohup java -jar $BACKEND_DIR/bff/build/libs/bff.jar 2>&1 &
